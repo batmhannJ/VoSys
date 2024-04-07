0948e9cc8a03ed1e646b774f5d525dc7ddf0f811
batmhannJ <reyeshannahjoy82@gmail.com>
2024-04-07 21:15:36 +0800

    admin database config update

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'votesystem');
+	$conn = new mysqli('localhost', 'vosys', 'vosysOlshco5', 'u247141684_votesystem');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
