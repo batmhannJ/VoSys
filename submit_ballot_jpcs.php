57d2078f6f93d88ccb054d66301dcb2c533e83cc
batmhannJ <reyeshannahjoy82@gmail.com>
2024-04-10 02:41:25 +0800

    database config line 34

--- submit_ballot_jpcs.php
+++ submit_ballot_jpcs.php
@@ -31,7 +31,7 @@
 					}
 					else{
 						$candidate = $_POST[$position];
-						$sql_array[] = "INSERT INTO votes (voters_id, candidate_id, position_id) VALUES ('".$voter['id']."', '$candidate', '$pos_id')";
+						$sql_array[] = "INSERT INTO votes (voters_id, candidate_id, position_id, organization) VALUES ('".$voter['id']."', '$candidate', '$pos_id', '".$voter['organization']."')";
 					}
 
 				}
