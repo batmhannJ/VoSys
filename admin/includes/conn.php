e22d774e1f5514568b761680d7283b2b55a10300
batmhannJ <reyeshannahjoy82@gmail.com>
2024-04-10 00:19:59 +0800

    connections

--- admin/includes/conn.php
+++ admin/includes/conn.php
@@ -1,17 +1,8 @@
 <?php
-    // Enable error reporting
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
+	$conn = new mysqli('localhost', 'u247141684_vosys', 'vosysOlshco5', 'u247141684_votesystem');
 
-    try {
-        // Enable MySQLi exceptions
-        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
 
-        // Create a connection to the database
-		$conn = new mysqli('localhost', 'u247141684_vosys', 'vosysOlshco5', 'u247141684_votesystem');
-
-        echo "Connected successfully"; // Display success message if no exceptions are thrown
-    } catch (Exception $e) {
-        echo "Connection failed: " . $e->getMessage(); // Display exception message if an exception occurs
-    }
 ?>
