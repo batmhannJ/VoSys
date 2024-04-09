073dd1768e98d2eeda1ba24351deed407ab85afb
batmhannJ <reyeshannahjoy82@gmail.com>
2024-04-10 00:08:05 +0800

    connection to hostinger

--- includes/conn.php
+++ includes/conn.php
@@ -8,7 +8,7 @@
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
         // Create a connection to the database
-		$conn = new mysqli('localhost', 'vosys', 'vosysOlshco5', 'u247141684_votesystem');
+		$conn = new mysqli('localhost', 'u247141684_vosys', 'vosysOlshco5', 'u247141684_votesystem');
 
         echo "Connected successfully"; // Display success message if no exceptions are thrown
     } catch (Exception $e) {
