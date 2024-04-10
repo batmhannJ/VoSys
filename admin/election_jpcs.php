1f7cac182d02a662d5834a589c08fc5caff15830
xantyyy <santypalma0@gmail.com>
2024-04-10 13:58:42 +0800

    jpcs menubar

--- admin/election_jpcs.php
+++ admin/election_jpcs.php
@@ -15,7 +15,7 @@
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Election Lists</li>
+        <li class="active">Election Lists </li>
       </ol>
     </section>
     <!-- Main content -->
