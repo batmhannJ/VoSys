291e1ebef577ba2b2072bd696a673e55d59ed78a
xantyyy <santypalma0@gmail.com>
2024-04-10 12:56:21 +0800

    jpcs menubar

--- admin/positions_jpcs.php
+++ admin/positions_jpcs.php
@@ -15,7 +15,7 @@
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Positions</li>
+        <li class="active">Positions </li>
       </ol>
     </section>
     <!-- Main content -->
