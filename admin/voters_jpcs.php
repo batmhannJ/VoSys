a1cea3b458bf3287e57fd6919bcfffe20a97bcac
batmhannJ <reyeshannahjoy82@gmail.com>
2024-04-12 01:27:05 +0800

    voters config for jpcs org

--- admin/voters_jpcs.php
+++ admin/voters_jpcs.php
@@ -130,7 +130,7 @@
   </div>
     
   <?php include 'includes/footer.php'; ?>
-  <?php include 'includes/voters_modal.php'; ?>
+  <?php include 'includes/voters_jpcs_modal.php'; ?>
 </div>
 <?php include 'includes/scripts.php'; ?>
 <script>
