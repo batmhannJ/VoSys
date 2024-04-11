a983854da6094b05fb434eaedc08faa4f912b464
batmhannJ <reyeshannahjoy82@gmail.com>
2024-04-12 00:36:17 +0800

    line 198

--- admin/home_jpcs.php
+++ admin/home_jpcs.php
@@ -195,7 +195,7 @@
               // Calculate and display the percentage
               if ($totalNumberOfVoters > 0) {
                   $percentage = ($totalRows / $totalNumberOfVoters) * 100;
-                  echo "<h3>" . $percentage . "%";
+                  echo "<h3>" . $percentage . "%" ."</h3>";
               } else {
                   echo "Total number of voters is 0. Cannot calculate percentage.";
               }
