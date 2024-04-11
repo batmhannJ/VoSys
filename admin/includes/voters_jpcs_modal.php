faca1234c60e8d42efb9fb53e8178e5a76628547
batmhannJ <reyeshannahjoy82@gmail.com>
2024-04-12 01:31:08 +0800

    minimal edits

--- admin/includes/voters_jpcs_modal.php
+++ admin/includes/voters_jpcs_modal.php
@@ -115,14 +115,14 @@
                     <label for="edit_password" class="col-sm-3 control-label">Password</label>
 
                     <div class="col-sm-9">
-                      <input type="password" class="form-control" id="edit_password" name="password">
+                        <input type="password" class="form-control" id="edit_password" name="password">
                     </div>
                 </div>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
-              <button type="submit" class="btn btn-success btn-flat" name="edit"><i class="fa fa-check-square-o"></i> Update</button>
-              </form>
+                <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+                <button type="submit" class="btn btn-success btn-flat" name="edit"><i class="fa fa-check-square-o"></i> Update</button>
+                </form>
             </div>
         </div>
     </div>
