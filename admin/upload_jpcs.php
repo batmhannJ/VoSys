559e1789a4c27c5d84a2c8f958ba30a5952d66b6
batmhannJ <reyeshannahjoy82@gmail.com>
2024-04-12 01:43:28 +0800

    displaying errors

--- admin/upload_jpcs.php
+++ admin/upload_jpcs.php
@@ -4,35 +4,35 @@ include 'includes/session.php';
 include 'includes/conn.php';
 
 require 'vendor/autoload.php';
-    require 'C:\Xampp\htdocs\votesystem\admin\PHPMailer\src\PHPMailer.php';
-    require 'C:\Xampp\htdocs\votesystem\admin\PHPMailer\src\SMTP.php';
-    require 'C:\Xampp\htdocs\votesystem\admin\PHPMailer\src\Exception.php';
+require 'C:\Xampp\htdocs\votesystem\admin\PHPMailer\src\PHPMailer.php';
+require 'C:\Xampp\htdocs\votesystem\admin\PHPMailer\src\SMTP.php';
+require 'C:\Xampp\htdocs\votesystem\admin\PHPMailer\src\Exception.php';
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\SMTP;
-    use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-if(isset($_POST['save_excel_data']))
-{
-    $fileName = $_FILES['import_file']['name'];
-    $file_ext = pathinfo($fileName, PATHINFO_EXTENSION);
+if (isset($_POST['save_excel_data'])) {
+    try {
+        $fileName = $_FILES['import_file']['name'];
+        $file_ext = pathinfo($fileName, PATHINFO_EXTENSION);
 
-    $allowed_ext = ['xls','csv','xlsx'];
+        $allowed_ext = ['xls', 'csv', 'xlsx'];
 
-    if(in_array($file_ext, $allowed_ext))
-    {
+        if (!in_array($file_ext, $allowed_ext)) {
+            throw new Exception("Please upload a xls, csv or xlsx file.");
+        }
+
         $inputFileNamePath = $_FILES['import_file']['tmp_name'];
         $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileNamePath);
         $data = $spreadsheet->getActiveSheet()->toArray();
 
-        $count = "0";
-        foreach($data as $row)
-        {
-            if($count > 0)
-            {
+        $count = 0;
+        foreach ($data as $row) {
+            if ($count > 0) {
                 $firstname = $row['0'];
                 $lastname = $row['1'];
                 $email = $row['2'];
@@ -40,8 +40,7 @@ if(isset($_POST['save_excel_data']))
                 $yearlvl = $row['4'];
 
                 if ($organization != 'JPCS') {
-                    echo "<script>alert('Organization must be JPCS.');</script>";
-                    continue; // Skip this iteration of the loop
+                    throw new Exception("Organization must be JPCS.");
                 }
 
                 $set = '1234567890';
@@ -56,32 +55,28 @@ if(isset($_POST['save_excel_data']))
 
                 $mail = new PHPMailer();
                 // Remainder of the code...
-            }
-            else
-            {
-                $count = "1";
+            } else {
+                $count = 1;
             }
         }
 
-        if(isset($msg))
-        {
+        if (isset($msg)) {
             $_SESSION['success'] = "Successfully Imported";
             header('Location: voters_jpcs.php');
             exit(0);
-        }
-        else
-        {
+        } else {
             $_SESSION['message'] = "Not Imported";
             header('Location: voters_jpcs.php');
             exit(0);
         }
-    }
-    else
-    {
-        $_SESSION['message'] = "Please upload a xls, csv or xlsx file.";
+    } catch (Exception $e) {
+        $_SESSION['error'] = $e->getMessage();
         header('Location: voters_jpcs.php');
         exit(0);
     }
+} else {
+    $_SESSION['message'] = "No data received.";
+    header('Location: voters_jpcs.php');
+    exit(0);
 }
-
 ?>
