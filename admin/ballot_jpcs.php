d3c754a926c301c8e862113f71fd054e330a3579
batmhannJ <reyeshannahjoy82@gmail.com>
2024-04-12 08:41:43 +0800

    configuration for alert

--- admin/ballot_jpcs.php
+++ admin/ballot_jpcs.php
@@ -3,7 +3,7 @@
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
 
-  <?php include 'includes/navbar.php'; ?>
+  <?php include 'includes/navbar_jpcs.php'; ?>
   <?php include 'includes/menubar_jpcs.php'; ?>
 
   <!-- Content Wrapper. Contains page content -->
