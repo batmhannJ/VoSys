34af77a167975f549bbddb546a4f518e54038c89
likeugh25 <lykarefugia8@gmail.com>
2024-04-16 07:02:48 +0800

    add landing page live poll

--- admin/home.php
+++ admin/home.php
@@ -213,6 +213,17 @@
         <!-- ./col -->
       </div>
 
+      <p>Live Poll</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-black-tie"></i>
+            </div>
+            <a href="livepoll.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+
       <!--<div class="row">
         <div class="col-xs-12">
           <h3>Votes Tally
