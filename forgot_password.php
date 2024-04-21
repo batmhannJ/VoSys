5538dc1f255b613ff9efc6a334a5bba021a68047
batmhannJ <reyeshannahjoy82@gmail.com>
2024-04-21 14:46:40 +0800

    landing page of forgot password

---
+++ forgot_password.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forgot Password</title>
+    <!-- Include your CSS files -->
+</head>
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-box-body">
+            <div class="login-logo">
+                <img src="images/olshco.png" class="olshco-logo" alt="College Voting System Logo">
+                <b>College Voting System</b>
+            </div>
+            <p class="login-box-msg">Forgot Password? Enter your email address to reset your password.</p>
+
+            <!-- Forgot password form -->
+            <form action="forgot_password.php" method="POST">
+                <div class="form-group has-feedback">
+                    <input type="email" class="form-control" name="email" placeholder="Email" required>
+                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat" name="resetPass">Reset Password</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
