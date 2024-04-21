9486a5d9f33a3b5af480d6397b8e2170f98fd878
batmhannJ <reyeshannahjoy82@gmail.com>
2024-04-21 23:11:09 +0800

    candidates row

--- admin/candidates_row.php
+++ admin/candidates_row.php
@@ -3,10 +3,16 @@
 
 	if(isset($_POST['id'])){
 		$id = $_POST['id'];
-		$sql = "SELECT *, candidates.id AS canid FROM candidates LEFT JOIN categories ON categories.id=categories.position_id WHERE categories.id = '$id'";
+		$sql = "SELECT *, candidates.id AS canid FROM candidates LEFT JOIN categories ON candidates.category_id = categories.id WHERE categories.election_id = 1 AND categories.id = '$id'";
 		$query = $conn->query($sql);
-		$row = $query->fetch_assoc();
+		
+		$candidates = array(); // Initialize an array to store candidates
+		
+		// Fetching all candidates related to the specified category
+		while($row = $query->fetch_assoc()) {
+			$candidates[] = $row; // Add each candidate to the array
+		}
 
-		echo json_encode($row);
+		echo json_encode($candidates); // Encode the array of candidates to JSON and echo it
 	}
 ?>
