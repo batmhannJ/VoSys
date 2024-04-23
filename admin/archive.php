d46dde0b3c8a7477c0b4a3036aa51ca416007d18
likeugh25 <lykarefugia8@gmail.com>
2024-04-24 00:45:57 +0800

    Archived modify

--- admin/archive.php
+++ admin/archive.php
@@ -79,7 +79,7 @@
                           <td>".$row['yearLvl']."</td>
                           <td>".$row['organization']."</td>
                           <td>
-                            <button class='btn btn-success btn-sm restore btn-flat' data-id='".$row['id']."'><i class='fa fa-reply'></i> Restore</button>
+                            <button class='btn btn-success btn-sm restore btn-flat' data-id='".$row['id']."' data-toggle='modal' data-target='#confirmationModal'><i class='fa fa-reply'></i> Restore</button>
                           </td>
                         </tr>
                       ";
@@ -118,15 +118,32 @@
         </div>
     </div>
 </div>
+<!-- Restore Confirmation Modal -->
+<div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmationModalLabel">Confirmation</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure you want to restore this voter?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary" id="submitBtn">Yes, Restore</button>
+            </div>
+        </div>
+    </div>
+</div>
 <script>
 $(function(){
   $(document).on('click', '.restore', function(e){
     e.preventDefault();
     var id = $(this).data('id');
-    $('#confirmationModal').modal('show'); // Show the confirmation modal
-    $('#submitBtn').on('click', function() {
-        restoreVoter(id);
-    });
+    $('#submitBtn').attr('data-id', id); // Set the data-id attribute for the "Restore" button
   });
 
   $(document).on('click', '.restore-all', function(e){
@@ -137,6 +154,12 @@ $(function(){
   $(document).on('click', '#confirmRestoreAll', function(e){
     e.preventDefault();
     restoreAllVoters();
+  });
+
+  $(document).on('click', '#submitBtn', function(e){
+    e.preventDefault();
+    var id = $(this).data('id');
+    restoreVoter(id);
   });
 
   function restoreVoter(id) {
