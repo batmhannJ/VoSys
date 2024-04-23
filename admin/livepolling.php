1fee5b56383e65c52e7b3ca574b4f962302536d8
likeugh25 <lykarefugia8@gmail.com>
2024-04-23 23:21:58 +0800

    BAR GRAP LAHAT NG ORG(try)

--- admin/livepolling.php
+++ admin/livepolling.php
@@ -120,7 +120,33 @@ include 'includes/header.php';
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
     // Function to generate bar graph
-function generateBarGraph(dataPoints, containerId) {
+function generateBarGraph(dataPoints, containerId, organization) {
+    var color;
+
+    // Set color based on organization
+    switch (organization) {
+        case 'JPCS':
+            color = "#4CAF50"; // Green
+            break;
+        case 'CSC':
+            color = "#000000"; // Black
+            break;
+        case 'CODE-TG':
+            color = "#800000"; // Maroon
+            break;
+        case 'YMF':
+            color = "#00008b"; // Dark Blue
+            break;
+        case 'HMSO':
+            color = "#cba328"; // Gold
+            break;
+        case 'PASOA':
+            color = "#e6cc00"; // Yellow
+            break;
+        default:
+            color = "#000000"; // Default to Black
+    }
+
     var chart = new CanvasJS.Chart(containerId, {
         animationEnabled: true,
         title:{
@@ -140,11 +166,12 @@ function generateBarGraph(dataPoints, containerId) {
         data: [{
             type: "bar", // Change type to "bar"
             dataPoints: dataPoints,
-            color: "#4CAF50" // Set the color to #4CAF50 for JPCS
+            color: color // Set the color based on organization
         }]
     });
     chart.render();
 }
+
 
 
     // Function to fetch updated data from the server
