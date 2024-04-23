e596823ad76282f0ed35ad8ad99a113415f37b2c
soobink <peronamarie9@gmail.com>
2024-04-24 02:51:42 +0800

    back to og

--- subadmin/config_save.php
+++ subadmin/config_save.php
@@ -1,33 +1,6 @@
 <?php
 	include 'includes/session.php';
-	<?php
-
-// Google API configuration
-define('GOOGLE_CLIENT_ID', '521331514441-7th1b1gp40k3ueueosk160thurbugqfn.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', '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');
-define('GOOGLE_REDIRECT_URL', 'https://vosys.org/voters_login.php');
-
-//Include Google Client Library for PHP autoload file
-require_once 'vendor/autoload.php';
-
-//Make object of Google API Client for call Google API
-$google_client = new Google_Client();
-
-//Set the OAuth 2.0 Client ID
-$google_client->setClientId(GOOGLE_CLIENT_ID);
-
-//Set the OAuth 2.0 Client Secret key
-$google_client->setClientSecret(GOOGLE_CLIENT_SECRET);
-
-//Set the OAuth 2.0 Redirect URI
-$google_client->setRedirectUri(GOOGLE_REDIRECT_URL);
-
-$google_client->addScope('email');
-$google_client->addScope('profile');
-
-//start session on web page
-session_start();
-?>
+	
 
 	$return = 'home.php';
 	if(isset($_GET['return'])){
