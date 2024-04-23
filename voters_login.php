2f45c6c5cd8d34230c4ef3db6cd49535b6cde9d3
LEVi19xx <charmainejoycecoloma02@gmail.com>
2024-04-24 04:07:02 +0800

    debug

--- voters_login.php
+++ voters_login.php
@@ -511,7 +511,7 @@ main.sign-up-mode .carousel {
                         <?php
                 if (isset($_SESSION['error'])) {
                     echo "
-                        <div class='callout callout-danger text-center mt20' style='width: 100%; max-width: 300px; margin: 0 auto;'>
+                        <div class='callout callout-danger text-center mt20' style='width: 300px;'>
                             <p>" . $_SESSION['error'] . "</p> 
                         </div>
                     ";
