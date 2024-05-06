c85eb9d072302a584a662a53b9706274aea903e3
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-07 00:15:53 +0800

    dwqa

--- code_home.php
+++ code_home.php
@@ -146,29 +146,30 @@
                                 }   
                                 ?>
                                 <div class="text-center">
-                                    <button type="button" class="btn btn-primary btn-flat" id="submitBtn"><i class="fa fa-check-square-o"></i> Submit</button>
-                                </div>
+    <button type="button" class="btn btn-primary btn-flat" id="submitBtn"><i class="fa fa-check-square-o"></i> Submit</button>
+</div>
 
-                                <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="confirmationModalLabel">Confirmation</h5>
-                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
+<div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmationModalLabel">Confirmation</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
 
-                                        <div class="modal-body">
-                                            <p>Are you sure you want to submit your vote?</p>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-success btn-flat" id="preview"><i class="fa fa-file-text"></i> Preview</button> 
-                                            <button type="submit" class="btn btn-primary" id="submitBtn" name="vote_code">Yes, Submit</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+            <div class="modal-body">
+                <p>Are you sure you want to submit your vote?</p>
+            </div>
+            
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success btn-flat" id="preview"><i class="fa fa-file-text"></i> Preview</button> 
+                <button type="submit" class="btn btn-primary" id="submitBtnModal" name="vote_code">Yes, Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
                             <style>
     /* Button style */
     #submitBtn {
