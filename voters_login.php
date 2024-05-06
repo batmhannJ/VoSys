49610cab69f2d4830fc0b6388a11f108a42f8f67
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-06 23:55:01 +0800

    cxsd

--- voters_login.php
+++ voters_login.php
@@ -519,7 +519,8 @@ main.sign-up-mode .carousel {
             </div>
 
                 <input type="submit" name="login" value="Sign In" class="sign-btn" style="font-size:15px;">
-                <input type="submit" name="login" value="Back to Homepage" class="back-btn" style="font-size:15px;">
+                <input type="button" value="Back to Homepage" class="back-btn" style="font-size:15px;" onclick="window.location.href = 'index.html';">
+
                         <?php
                 if (isset($_SESSION['error'])) {
                     echo "
