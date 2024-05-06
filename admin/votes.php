21ee7496f9b6af5ddbee1c0ffbb630408b825008
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-06 23:38:48 +0800

    checking error

--- admin/votes.php
+++ admin/votes.php
@@ -1,3 +1,10 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+?>
+
 <?php include 'includes/session.php'; ?>
 <?php include 'includes/header.php'; ?>
 <body class="hold-transition skin-blue sidebar-mini">
