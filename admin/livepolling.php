e85a9ab08572d444f6e357e6063dbb097b0751f3
soobink1 <peronamarie9@gmail.com>
2024-05-07 18:06:28 +0800

    back og

--- admin/livepolling.php
+++ admin/livepolling.php
@@ -153,9 +153,14 @@ include 'includes/header.php';
                 text: "Vote Counts"
             },
             axisY: {
-                title: "Candidates",
-                includeZero: true
-            },
+                 title: "Candidates",
+                includeZero: true,
+                 labelFormatter: function (e) {
+        // Include candidate name and round vote count to whole number
+        return dataPoints[e.value].label + " - " + Math.round(e.value);
+    }
+},
+
             axisX: {
                 title: "Vote Count",
                 includeZero: true
