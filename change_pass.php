0b801caf5ce4da9623e0c9d96e14502dbdb923c7
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-07 23:41:45 +0800

    script added

--- change_pass.php
+++ change_pass.php
@@ -7,51 +7,71 @@ ini_set('display_errors', 1);
 session_start();
 ?>
 
+<?php include 'includes/header.php'; ?>
 
-<?php
-include 'includes/header.php'; 
-?>
 <body class="hold-transition login-page">
-<div class="login-box">
-    <div class="login-box-body">
-        <div class="login-logo">
-            <img src="images/olshco.png" class="olshco-logo" alt="College Voting System Logo">
-            <b>College Voting System</b>
-        </div>
-        <p class="login-box-msg">Change Password</p>
-        <!-- Password reset form -->
-        <form action="update_password.php" method="POST" onsubmit="return validateForm()">
-            <input type="hidden" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($user['email']); ?>" required>
-            <div class="form-group has-feedback">
-                <label for="new_password">New Password:</label>
-                <input type="password" class="form-control" id="new_password" name="new_password" required>
+    <div class="login-box">
+        <div class="login-box-body">
+            <div class="login-logo">
+                <img src="images/olshco.png" class="olshco-logo" alt="College Voting System Logo">
+                <b>College Voting System</b>
             </div>
-            <div class="form-group has-feedback">
-                <label for="confirm_password">Confirm Password:</label>
-                <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
-            </div>
-            <div class="row">
-                <div class="col-xs-12">
-                    <button type="submit" class="btn btn-primary" name="reset">Reset Password</button>
+            <p class="login-box-msg">Change Password</p>
+            <!-- Password reset form -->
+            <form id="password_reset_form" method="POST">
+                <input type="hidden" class="form-control" id="email" name="email" value="<?php echo htmlspecialchars($user['email']); ?>" required>
+                <div class="form-group has-feedback">
+                    <label for="new_password">New Password:</label>
+                    <input type="password" class="form-control" id="new_password" name="new_password" required>
                 </div>
-            </div>
-        </form>
+                <div class="form-group has-feedback">
+                    <label for="confirm_password">Confirm Password:</label>
+                    <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <button type="button" class="btn btn-primary" onclick="resetPassword()" name="reset">Reset Password</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
-</div>
 
-<?php include 'includes/scripts.php' ?>
+    <?php include 'includes/scripts.php' ?>
 
-<script>
-    function validateForm() {
-        var newPassword = document.getElementById("new_password").value;
-        var confirmPassword = document.getElementById("confirm_password").value;
+    <script>
+        function validateForm() {
+            var newPassword = document.getElementById("new_password").value;
+            var confirmPassword = document.getElementById("confirm_password").value;
 
-        if (newPassword !== confirmPassword) {
-            alert("Password and confirm password do not match");
-            return false;
+            if (newPassword !== confirmPassword) {
+                alert("Password and confirm password do not match");
+                return false;
+            }
+            return true;
         }
-        return true;
-    }
-</script>
+
+        function resetPassword() {
+            if (validateForm()) {
+                var form = document.getElementById("password_reset_form");
+                var formData = new FormData(form);
+
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "update_password.php", true);
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === XMLHttpRequest.DONE) {
+                        if (xhr.status === 200) {
+                            // Request was successful, handle response
+                            alert(xhr.responseText); // You can customize this part to display a success message or redirect to another page
+                        } else {
+                            // Request failed
+                            alert("Failed to update password");
+                        }
+                    }
+                };
+                xhr.send(formData);
+            }
+        }
+    </script>
 </body>
 </html>
