f1367f89016163fbf8d88912d677e2ad7bac53b2
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-07 18:36:12 +0800

    fsdf

--- code_home.php
+++ code_home.php
@@ -78,275 +78,74 @@
                             ?>
                             <!-- Voting Ballot -->
                             <form method="POST" id="ballotForm" action="submit_ballot_code.php">
-    <?php
-    include 'includes/slugify.php';
+                                <?php
+                                include 'includes/slugify.php';
 
-    $sql = "SELECT * FROM positions ORDER BY priority ASC";
-    $query = $conn->query($sql);
-    while($row = $query->fetch_assoc()){
-        echo '
-        <div class="position-container">
-            <div class="box box-solid" id="'.$row['id'].'">
-                <div class="box-header">
-                    <h3 class="box-title">'.$row['description'].'</h3>
-                    <button type="button" class="btn btn-success btn-sm btn-flat reset" data-desc="'.slugify($row['description']).'"><i class="fa fa-refresh"></i> Reset</button>
-                </div>
-                <div class="box-body">
-                    <p class="instruction">You may select up to '.$row['max_vote'].' candidates</p>
-                    <div class="candidate-list">
-                        <ul>';
-                            $sql_candidates = "SELECT * FROM candidates WHERE position_id='".$row['id']."'";
-                            $cquery = $conn->query($sql_candidates);
-                            while($crow = $cquery->fetch_assoc()){
-                                $slug = slugify($row['description']);
-                                $checked = '';
-                                if(isset($_SESSION['post'][$slug])){
-                                    $value = $_SESSION['post'][$slug];
-                                    if(is_array($value)){
-                                        foreach($value as $val){
-                                            if($val == $crow['id']){
-                                                $checked = 'checked';
+                                $candidate = '';
+                                $sql = "SELECT * FROM positions ORDER BY priority ASC";
+                                $query = $conn->query($sql);
+                                while($row = $query->fetch_assoc()){
+                                    $sql = "SELECT * FROM candidates WHERE position_id='".$row['id']."'";
+                                    $cquery = $conn->query($sql);
+                                    while($crow = $cquery->fetch_assoc()){
+                                        $slug = slugify($row['description']);
+                                        $checked = '';
+                                        if(isset($_SESSION['post'][$slug])){
+                                            $value = $_SESSION['post'][$slug];
+
+                                            if(is_array($value)){
+                                                foreach($value as $val){
+                                                    if($val == $crow['id']){
+                                                        $checked = 'checked';
+                                                    }
+                                                }
+                                            }
+                                            else{
+                                                if($value == $crow['id']){
+                                                    $checked = 'checked';
+                                                }
                                             }
                                         }
+                                        $input = ($row['max_vote'] > 1) ? '<input type="checkbox" class="flat-red '.$slug.'" name="'.$slug."[]".'" value="'.$crow['id'].'" '.$checked.'>' : '<input type="radio" class="flat-red '.$slug.'" name="'.slugify($row['description']).'" value="'.$crow['id'].'" '.$checked.'>';
+                                        $image = (!empty($crow['photo'])) ? 'images/'.$crow['photo'] : 'images/profile.jpg';
+                                        $candidate .= '
+                                            <li>
+                                                '.$input.'<button type="button" class="btn btn-primary btn-sm btn-flat clist platform" data-platform="'.$crow['platform'].'" data-fullname="'.$crow['firstname'].' '.$crow['lastname'].'"><i class="fa fa-search"></i> Platform</button><img src="'.$image.'" height="100px" width="100px" class="clist"><span class="cname clist">'.$crow['firstname'].' '.$crow['lastname'].'</span>
+                                            </li>
+                                        ';
                                     }
-                                    else{
-                                        if($value == $crow['id']){
-                                            $checked = 'checked';
-                                        }
-                                    }
-                                }
-                                $input = ($row['max_vote'] > 1) ? '<input type="checkbox" class="flat-red '.$slug.'" name="'.$slug."[]".'" value="'.$crow['id'].'" '.$checked.'>' : '<input type="radio" class="flat-red '.$slug.'" name="'.slugify($row['description']).'" value="'.$crow['id'].'" '.$checked.'>';
-                                $image = (!empty($crow['photo'])) ? 'images/'.$crow['photo'] : 'images/profile.jpg';
-                                echo '
-                                <li>
-                                    <div class="candidate-info">
-                                        '.$input.'
-                                        <span class="cname">'.$crow['firstname'].' '.$crow['lastname'].'</span>
-                                        
-                                    </div>
-                                    <button type="button" class="btn btn-primary btn-sm btn-flat platform" data-platform="'.$crow['platform'].'" data-fullname="'.$crow['firstname'].' '.$crow['lastname'].'">
-                                    <i class="fa fa-search"> Platform</i>
-                                </button>
-                                
-                                    <img src="'.$image.'" alt="'.$crow['firstname'].' '.$crow['lastname'].'" class="clist">
-                                </li>';
-                            }
-                        echo '</ul>
-                    </div>
-                </div>
-            </div>
-        </div>';
-    }
-    ?>
-</form>
-<style>
- /* Style for the position container */
-.position-container {
-    margin: 20px auto; /* Center the container horizontally and add margin on top and bottom */
-    max-width: 800px; /* Set a maximum width to make it responsive */
-    padding: 20px; /* Add padding inside the container */
-    border: 1px solid #ccc; /* Add border for visual separation */
-    border-radius: 10px; /* Add border radius for rounded corners */
-    background-color: #fff; /* Change background color */
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.9); /* Add shadow for depth */
-}
 
-/* Style for the box header */
-.box-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #800000;
-    color: #fff;
-    padding: 10px;
-}
+                                    $instruct = ($row['max_vote'] > 1) ? 'You may select up to '.$row['max_vote'].' candidates' : 'Select only one candidate';
 
-/* Adjust space between position and reset button */
-.box-header .box-title {
-    margin-right: auto; /* Push position title to the left */
-}
+                                    echo '
+                                        <div class="row">
+                                            <div class="col-xs-12">
+                                                <div class="box box-solid" id="'.$row['id'].'">
+                                                    <div class="box-header with-border">
+                                                        <h3 class="box-title"><b>'.$row['description'].'</b></h3>
+                                                    </div>
+                                                    <div class="box-body">
+                                                        <p>'.$instruct.'
+                                                            <span class="pull-right">
+                                                                <button type="button" class="btn btn-success btn-sm btn-flat reset" data-desc="'.slugify($row['description']).'"><i class="fa fa-refresh"></i> Reset</button>
+                                                            </span>
+                                                        </p>
+                                                        <div id="candidate_list">
+                                                            <ul>
+                                                                '.$candidate.'
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ';
 
-/* Style for the reset button */
-.reset {
-    margin-left: auto; /* Push reset button to the right */
-}
+                                    $candidate = '';
 
-/* Style for the box title */
-.box-title {
-    margin: 0;
-    font-size: 20px;
-    font-weight: 300;
-}
-
-/* Style for the box body */
-.box-body {
-    padding: 10px;
-}
-
-/* Style for the voting instructions */
-.instruction {
-    font-size: 16px;
-    margin-bottom: 10px;
-}
-
-/* Style for the candidate list */
-.candidate-list ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-/* Bagong istilo para sa mga item sa listahan ng mga kandidato */
-.candidate-list li {
-    display: flex; /* Baguhin ang display sa flex */
-    flex-wrap: wrap; /* Pahintulutan ang pag-wrap ng mga item sa loob ng flex container */
-    justify-content: space-between; /* I-set ang mga item na sa layong pare-pareho */
-    align-items: center; /* I-align ang mga item sa gitna */
-    border-radius: 5px; /* Radius ng border */
-    padding: 10px; /* Padding para sa mga item */
-    margin-bottom: 10px; /* Espasyo sa pagitan ng mga item */
-    background-color: #f9f9f9; /* Kulay ng background */
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1); /* Shadow para sa depth */
-}
-
-/* Media query para sa mas maliit na mga screen */
-@media (max-width: 768px) {
-    .platform {
-        padding: 6px 16px; /* I-adjust ang padding para sa mas maliit na screen */
-        font-size: 14px; /* I-adjust ang font size */
-        width: auto; /* I-adjust ang lapad */
-    }
-    .candidate-list li {
-        flex-direction: column; /* Baguhin ang direksyon ng flex container sa column */
-        align-items: center; /* I-align ang mga item sa gitna */
-        padding: 15px; /* I-adjust ang padding para sa mas maliit na screen */
-    }
-
-    .candidate-list li img {
-        width: 100px; /* I-adjust ang lapad ng mga larawan para sa mas maliit na screen */
-        height: 100px; /* I-adjust ang taas ng mga larawan para sa mas maliit na screen */
-        margin: 0 auto; /* Ilipat ang mga larawan sa gitna */
-    }
-}
-
-
-
-/* Adjusted style for candidate name */
-.cname {
-    font-size: 18px; /* Default font size */
-    margin-left: auto; /* Push candidate name to the end */
-    font-weight: bold;
-}
-
-/* Media query for smaller screens */
-@media (max-width: 768px) {
-    .cname {
-        font-size: 15px; /* Reduce font size on smaller screens */
-    }
-}
-
-/* Adjusted style for platform button */
-.platform {
-    background-color: #007bff;
-    color: #fff;
-    border: none;
-    border-radius: 20px; /* Make it pill-shaped */
-    padding: 8px 20px; /* Add padding */
-    cursor: pointer;
-    transition: background-color 0.3s ease;
-    margin-left: auto; /* Push platform button to the end */
-    display: flex; /* Use flexbox to align icon and text */
-    align-items: center; /* Center items vertically */
-}
-
-.platform:hover {
-    background-color: #0056b3;
-}
-
-.platform i {
-    font-style: normal; /* Ibalik ang font style sa normal */
-    font-weight: bold; /* I-set ang font weight sa bold */
-    font-size: 14px; /* I-adjust ang font size */
-}
-
-/* Media query for smaller screens */
-@media (max-width: 768px) {
-  
-    .platform {
-        padding: 6px 16px; /* I-adjust ang padding para sa mas maliit na screen */
-        font-size: 14px; /* I-adjust ang font size */
-        width: auto; /* I-adjust ang lapad */
-        margin: 10px auto; /* Igitna ang platform button */
-    }
-
-    .platform i.fa {
-        margin-right: 0; /* Remove right margin for icon */
-    }
-
-    .platform span.text {
-        display: none; /* Hide text on smaller screens */
-    }
-}
-
-/* Updated styles for candidate image */
-.clist {
-    width: 100px;
-    height: 100px;
-    object-fit: cover;
-    border-radius: 50%;
-    margin-right: 10px;
-    grid-column: span 1;
-}
-
-/* Media query for smaller screens */
-@media (max-width: 768px) {
-    .position-container {
-        padding: 10px; /* Adjust padding for smaller screens */
-    }
-}
-
-/* Media query for larger screens */
-@media (min-width: 768px) {
-    /* Apply flex-end alignment to candidate image */
-    .candidate-list li {
-        display: flex;
-        justify-content: space-between; /* Align items to the end of the container */
-        align-items: center;
-        border: 1px solid #ccc;
-        border-radius: 5px;
-        padding: 10px;
-        margin-bottom: 10px;
-        background-color: #f9f9f9;
-    }
-
-    /* Updated styles for candidate image */
-    .clist {
-        width: 100px;
-        height: 100px;
-        object-fit: cover;
-        border-radius: 50%;
-        margin-right: 10px;
-        grid-column: span 1;
-    }
-
-    /* Media query for smaller screens */
-    @media (max-width: 768px) {
-        .candidate-list li {
-            display: flex;
-            align-items: center; /* Center items vertically */
-            margin-bottom: 10px;
-        }
-
-        .clist {
-            width: 80px; /* Reduce image width on smaller screens */
-            height: 80px; /* Reduce image height on smaller screens */
-            margin-right: 10px; /* Adjust margin for smaller screens */
-        }
-    }
-}
-
-</style>
-
-<div class="text-center">
+                                }   
+                                ?>
+                                <div class="text-center">
                                     <button type="button" class="btn btn-primary btn-flat" id="submitBtn"><i class="fa fa-check-square-o"></i> Submit</button>
                                 </div>
 
@@ -370,9 +169,8 @@
                                     </div>
                                 </div>
                             </div>
-                            </form>
-                      
 
+                            </form>
 
 				        	<!-- End Voting Ballot -->
 				    		<?php
