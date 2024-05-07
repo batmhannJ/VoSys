ee676fc9381c0c35f1d1cb7ec2e95f2214733783
https.xntyyy <santypalma0@gmail.com>
2024-05-07 16:59:56 +0800

    qweqweqwe

--- admin/elections.php
+++ admin/elections.php
@@ -20,83 +20,59 @@
     </section>
     <!-- Main content -->
     <section class="content">
-      <?php
-        if(isset($_SESSION['error'])){
-          echo "
-            <div class='alert alert-danger alert-dismissible'>
-              <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-              <h4><i class='icon fa fa-warning'></i> Error!</h4>
-              ".$_SESSION['error']."
-            </div>
-          ";
-          unset($_SESSION['error']);
-        }
-        if(isset($_SESSION['success'])){
-          echo "
-            <div class='alert alert-success alert-dismissible'>
-              <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-              <h4><i class='icon fa fa-check'></i> Success!</h4>
-              ".$_SESSION['success']."
-            </div>
-          ";
-          unset($_SESSION['success']);
-        }
-      ?>
-  <div class="row">
-    <div class="col-xs-12">
-    <div class="box">
-      <div class="box-header with-border">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header with-border">
               <a href="#addElection" data-toggle="modal" class="btn btn-primary btn-sm btn-flat"><i class="fa fa-plus"></i> New</a>
+            </div>
+            <!-- Table with hoverable rows -->
+            <div class="box-body">
+              <table id="example1" class="table table-bordered">
+                <thead>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Voters</th>
+                    <th scope="col">Status</th>
+                    <th class="text-center" scope="col">Action</th>
+                </thead>
+                <tbody class="election">
+                  <?php
+                  $i = 1;
+                  $election = $conn->prepare("SELECT * FROM election ORDER BY id DESC");
+                  $election->execute();
+                  $result = $election->get_result();
+                  while ($row = $result->fetch_assoc()) {
+
+                    echo '<tr>
+                            <th scope="row">' . $i++ . '</th>
+                            <td>'.$row['title'].'</td>
+                            <td>' . $row['voters'];
+                    '</td>';
+                    if ($row['status'] === 0) {
+                      echo '<td><a href="#" name="status" class="btn badge rounded-pill btn-secondary election-status" data-id="' . $row['id'] . '" data-status="1" data-name="Activate">Not active</a></td>';
+                    } else {
+                      echo '<td><a href="#" name="status" class="btn badge rounded-pill btn-success election-status" data-id="' . $row['id'] . '" data-status="0" data-name="Deactivate">Active</a></td>';
+                    }
+                    echo '<td class="text-center">
+                          <a href="#" class="btn btn-primary btn-sm edit btn-flat" data-bs-toggle="modal" data-bs-target="#editElection" data-id="' . $row['id'] . '">Edit</a>
+                          <a href="#" class="btn btn-warning btn-sm archive btn-flat" data-bs-toggle="modal" data-bs-target="#archiveElection" data-id="' . $row['id'] . '" data-name="' . $row['title'] . '">Archive</a></td>
+                        </tr>';    
+                  } ?>
+                </tbody>
+              </table><!-- End Election lists Table -->
+
+            </div>
+          </div>
+        </div>
       </div>
-      <!-- Table with hoverable rows -->
-      <div class="box-body">
-      <table id="example1" class="table table-bordered">
-        <thead>
-            <th scope="col">#</th>
-            <th scope="col">Title</th>
-            <th scope="col">Voters</th>
-            <th scope="col">Status</th>
-            <th class="text-center" scope="col">Action</th>
-        </thead>
-        <tbody class="election">
-          <?php
-          $i = 1;
-          $election = $conn->prepare("SELECT * FROM election ORDER BY id DESC");
-          $election->execute();
-          $result = $election->get_result();
-          while ($row = $result->fetch_assoc()) {
-
-            echo '<tr>
-                    <th scope="row">' . $i++ . '</th>
-                    <td>'.$row['title'].'</td>
-                    <td>' . $row['voters'];
-            '</td>';
-            if ($row['status'] === 0) {
-              echo '<td><a href="#" name="status" class="btn badge rounded-pill btn-secondary election-status" data-id="' . $row['id'] . '" data-status="1" data-name="Activate">Not active</a></td>';
-            } else {
-              echo '<td><a href="#" name="status" class="btn badge rounded-pill btn-success election-status" data-id="' . $row['id'] . '" data-status="0" data-name="Deactivate">Active</a></td>';
-            }
-            echo '<td class="text-center">
-            <a href="#" class="btn btn-primary btn-sm edit btn-flat" data-bs-toggle="modal" data-bs-target="#editElection" data-id="' . $row['id'] . '">Edit</a>
-            <a href="#" class="btn btn-warning btn-sm archive btn-flat" data-bs-toggle="modal" data-bs-target="#archiveElection" data-id="' . $row['id'] . '" data-name="' . $row['title'] . '">Archive</a></td>
-          </tr>';    
-          } ?>
-        </tbody>
-      </table><!-- End Election lists Table -->
-
-    </div>
+    </section>
   </div>
-</div>
-</section>
-</div>
 
   <?php include 'includes/footer.php'; ?>
   <?php include 'includes/election_modal.php'; ?>
 </div>
 <?php include 'includes/scripts.php'; ?>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
 <script>
 $(function(){
   $(document).on('click', '.edit', function(e){
@@ -105,14 +81,6 @@ $(function(){
     var id = $(this).data('id');
     getRow(id);
   });
-
-  $(document).on('click', '.delete', function(e){
-    e.preventDefault();
-    $('#delete').modal('show');
-    var id = $(this).data('id');
-    getRow(id);
-  });
-
 
 });
 function getRow(id){
@@ -132,56 +100,55 @@ function getRow(id){
     }
   });
 }
-    $(function () {
-        $('#starttime').datetimepicker();
-        $('#endtime').datetimepicker();
-    });
+$(function () {
+    $('#starttime').datetimepicker();
+    $('#endtime').datetimepicker();
+});
 
 
-  $(document).on('click', '.election-status', function(e) {
-    e.preventDefault();
+$(document).on('click', '.election-status', function(e) {
+  e.preventDefault();
 
-    var electionId = $(this).data('id');
-    var status = $(this).data('status');
-    var statusName = $(this).data('name'); // Corrected attribute name
+  var electionId = $(this).data('id');
+  var status = $(this).data('status');
+  var statusName = $(this).data('name'); // Corrected attribute name
 
-    var confirmed = confirm('Are you sure you want to ' + statusName + ' this Election?');
+  var confirmed = confirm('Are you sure you want to ' + statusName + ' this Election?');
 
-    // If the user confirms, proceed with change election status
-    if (confirmed) {
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost/votesystem/admin/controllers/app.php?action=election_status',
-            data: {
-                election_id: electionId,
-                status: status
-            },
-            dataType: 'json',
-            beforeSend: function() {
-                showLoadingOverlay();
-            },
-            success: function(response) {
-                console.log(response);
-                if (response.status === 'success') {
-                    toastr.success(response.message);
-                    setTimeout(function() {
-                        location.reload();
-                    }, 1000);
-                } else {
-                    toastr.error(response.message);
-                }
-            },
-            error: function(xhr, status, error) {
-                // Handle AJAX errors, if any
-                console.error(error);
-                hideLoadingOverlay();
-                console.error('An error occurred during the request.', status, error);
-            }
-        });
-    } else {
-        // User canceled to change status
-        toastr.info('Status change canceled.');
-    }
+  // If the user confirms, proceed with change election status
+  if (confirmed) {
+      $.ajax({
+          type: 'POST',
+          data: {
+              election_id: electionId,
+              status: status
+          },
+          dataType: 'json',
+          beforeSend: function() {
+              showLoadingOverlay();
+          },
+          success: function(response) {
+              console.log(response);
+              if (response.status === 'success') {
+                  toastr.success(response.message);
+                  setTimeout(function() {
+                      location.reload();
+                  }, 1000);
+              } else {
+                  toastr.error(response.message);
+              }
+          },
+          error: function(xhr, status, error) {
+              // Handle AJAX errors, if any
+              console.error(error);
+              hideLoadingOverlay();
+              console.error('An error occurred during the request.', status, error);
+          }
+      });
+  } else {
+      // User canceled to change status
+      toastr.info('Status change canceled.');
+  }
 });
 
 </script>
