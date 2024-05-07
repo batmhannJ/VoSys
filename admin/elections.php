807d1eb8df7fe3bb5d726213a46e1cf55bf22bd0
https.xntyyy <santypalma0@gmail.com>
2024-05-07 17:38:41 +0800

    zxzx

--- admin/elections.php
+++ admin/elections.php
@@ -204,4 +204,26 @@ $(document).on('click', '#confirmArchive', function() {
     });
 });
 </script>
+
+<!-- Confirmation Modal for Archive -->
+<div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmationModalLabel">Archive Election</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Are you sure you want to archive this election?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary" id="confirmArchive">Archive</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
