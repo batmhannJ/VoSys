61c4971300c0f50289af34cce069e89512816af5
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-08 00:18:17 +0800

    error log

--- update_password.php
+++ update_password.php
@@ -2,11 +2,19 @@
 
 include 'includes/session.php';
 
+// Debugging statement
+error_log("POST data: " . print_r($_POST, true));
+
 if (isset($_POST['reset'])) {
     // Get the form data
     $email = $_POST['email'];
     $password = $_POST['new_password'];
     $confirm_password = $_POST['confirm_password'];
+
+    // Debugging statement
+    error_log("Email: " . $email);
+    error_log("Password: " . $password);
+    error_log("Confirm Password: " . $confirm_password);
 
     // Check if the entered password matches the confirm password
     if ($password != $confirm_password) {
@@ -17,6 +25,9 @@ if (isset($_POST['reset'])) {
     } else {
         // Hash the new password
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+        // Debugging statement
+        error_log("Hashed Password: " . $hashed_password);
 
         // Establish database connection (assuming $conn is your database connection)
         include 'includes/conn.php'; // Adjust the filename as per your actual file
@@ -54,6 +65,9 @@ if (isset($_POST['reset'])) {
         'message' => 'Invalid request'
     ];
 }
+
+// Debugging statement
+error_log("Response: " . print_r($response, true));
 
 // Output the response as JSON
 header('Content-Type: application/json');
