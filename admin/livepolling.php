48369cbabbd181fae6ae7ac8e2326f65bfea4e1e
soobink1 <peronamarie9@gmail.com>
2024-05-07 18:16:05 +0800

    garapal

--- admin/livepolling.php
+++ admin/livepolling.php
@@ -45,11 +45,8 @@
       </div>
       <div class="row">
         <div class="col-xs-12">
-          <div class="col-xs-6">
+          <div class="col-xs-12">
             <div id="presidentChart" style="height: 370px; width: 100%; margin-left: 20px; margin-top: 20px; display: inline-block;"></div>
-          </div>
-          <div class="col-xs-6">
-            <div id="representativeChart" style="height: 370px; width: 100%; margin-left: 20px; margin-top: 20px; display: inline-block;"></div>
           </div>
         </div>
       </div>
@@ -74,146 +71,85 @@
     var organization = document.getElementById("organization").value;
 
     updatePresidentChart(organization);
-    updateRepresentativeChart(organization);
   }
 
   function updatePresidentChart(organization) {
-    var dataPoints = [];
+    var presidentDataPoints = [];
+    var vicePresidentDataPoints = [];
 
-    // You can fetch data dynamically based on the selected organization here
+    // Fetch data dynamically based on the selected organization
+
     // For demonstration, I'm using static data for each organization
-
     if (organization === "JPCS") {
-      dataPoints = [
-        { y: 20, label: "President" },
-        { y: 30, label: "Vice President" },
-        { y: 40, label: "Secretary" }
-      ];
-    } else if (organization === "PASOA") {
-      dataPoints = [
-        { y: 25, label: "President" },
-        { y: 35, label: "Vice President" },
-        { y: 45, label: "Secretary" }
-      ];
-    } else if (organization === "CSC") {
-      dataPoints = [
-        { y: 35, label: "President" },
-        { y: 25, label: "Vice President" },
-        { y: 40, label: "Secretary" }
-      ];
-    } else if (organization === "YMF") {
-      dataPoints = [
-        { y: 30, label: "President" },
-        { y: 40, label: "Vice President" },
-        { y: 30, label: "Secretary" }
-      ];
-    } else if (organization === "CODE-TG") {
-      dataPoints = [
-        { y: 20, label: "President" },
-        { y: 30, label: "Vice President" },
-        { y: 50, label: "Secretary" }
-      ];
-    } else if (organization === "HMSO") {
-      dataPoints = [
-        { y: 15, label: "President" },
-        { y: 35, label: "Vice President" },
-        { y: 50, label: "Secretary" }
-      ];
+        presidentDataPoints = [
+            { y: 20, label: "President" }
+        ];
+        vicePresidentDataPoints = [
+            { y: 30, label: "Vice President" }
+        ];
     }
     // Add more else if conditions for other organizations
 
     var chart = new CanvasJS.Chart("presidentChart", {
-      title: { text: "President, Vice President, Secretary" },
-      data: [{
-        type: "bar",
-        dataPoints: dataPoints,
-        color: organizationColors[organization] // Set organization-specific color
-      }]
+        title: { text: "President and Vice President" },
+        axisY: {
+            title: "Votes"
+        },
+        toolTip: {
+            shared: true
+        },
+        legend: {
+            cursor: "pointer",
+            verticalAlign: "top",
+            itemclick: function(e) {
+                if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+                    e.dataSeries.visible = false;
+                } else {
+                    e.dataSeries.visible = true;
+                }
+                e.chart.render();
+            }
+        },
+        data: [{
+            type: "bar",
+            showInLegend: true,
+            name: "President",
+            dataPoints: presidentDataPoints,
+            color: organizationColors[organization]
+        },
+        {
+            type: "bar",
+            showInLegend: true,
+            name: "Vice President",
+            dataPoints: vicePresidentDataPoints,
+            color: organizationColors[organization]
+        }]
     });
 
     chart.render();
 
     // Update chart every second
     setInterval(function () {
-      updatePresidentDataPoints(organization, chart);
+        updatePresidentDataPoints(organization, chart);
     }, 1000);
-  }
-
-  function updateRepresentativeChart(organization) {
-    var dataPoints = [];
-
-    // You can fetch data dynamically based on the selected organization here
-    // For demonstration, I'm using static data for each organization
-
-    if (organization === "JPCS") {
-      dataPoints = [
-        { y: 30, label: "Representative 1" },
-        { y: 40, label: "Representative 2" }
-      ];
-    } else if (organization === "PASOA") {
-      dataPoints = [
-        { y: 20, label: "Representative 1" },
-        { y: 30, label: "Representative 2" }
-      ];
-    } else if (organization === "CSC") {
-      dataPoints = [
-        { y: 25, label: "Representative 1" },
-        { y: 35, label: "Representative 2" }
-      ];
-    } else if (organization === "YMF") {
-      dataPoints = [
-        { y: 30, label: "Representative 1" },
-        { y: 40, label: "Representative 2" }
-      ];
-    } else if (organization === "CODE-TG") {
-      dataPoints = [
-        { y: 20, label: "Representative 1" },
-        { y: 30, label: "Representative 2" }
-      ];
-    } else if (organization === "HMSO") {
-      dataPoints = [
-        { y: 15, label: "Representative 1" },
-        { y: 25, label: "Representative 2" }
-      ];
-    }
-    // Add more else if conditions for other organizations
-
-    var chart = new CanvasJS.Chart("representativeChart", {
-      title: { text: "Representatives" },
-      data: [{
-        type: "bar",
-        dataPoints: dataPoints,
-        color: organizationColors[organization] // Set organization-specific color
-      }]
-    });
-
-    chart.render();
-
-    // Update chart every second
-    setInterval(function () {
-      updateRepresentativeDataPoints(organization, chart);
-    }, 1000);
-  }
+}
 
   function updatePresidentDataPoints(organization, chart) {
     // Update dataPoints based on the selected organization
     // For demonstration, I'm using random values for each data point
-    var newDataPoints = [];
+    var newPresidentDataPoints = [];
+    var newVicePresidentDataPoints = [];
+    
     for (var i = 0; i < chart.options.data[0].dataPoints.length; i++) {
-      newDataPoints.push({ label: chart.options.data[0].dataPoints[i].label, y: Math.random() * 100 });
+      newPresidentDataPoints.push({ label: "President", y: Math.random() * 100 });
     }
-    chart.options.data[0].dataPoints = newDataPoints;
-    chart.render();
-  }
 
-  function updateRepresentativeDataPoints(organization, chart) {
-    // Update dataPoints based on the selected organization
-    // For demonstration, I'm using random values for each data point
-    var newDataPoints = [];
-    for (var i = 0; i < chart.options.data[0].dataPoints.length; i++) {
-      newDataPoints.push({ label: chart.options.data[0].dataPoints[i].label, y: Math.random() * 100 });
+    for (var i = 0; i < chart.options.data[1].dataPoints.length; i++) {
+      newVicePresidentDataPoints.push({ label: "Vice President", y: Math.random() * 100 });
     }
-    chart.options.data[0].dataPoints = newDataPoints;
+    
+    chart.options.data[0].dataPoints = newPresidentDataPoints;
+    chart.options.data[1].dataPoints = newVicePresidentDataPoints;
     chart.render();
   }
 
