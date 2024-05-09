2853271e62d99b88b45e79ec7c48b2a02c4aa618
soobink1 <peronamarie9@gmail.com>
2024-05-09 21:24:24 +0800

    back og

--- admin/livepolling.php
+++ admin/livepolling.php
@@ -1,209 +1,191 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-type" content="text/html; charset=utf-8">
-        <title>Testing Dual Scale Bar Graph</title>
-        <script type="text/javascript" src="js/d3.v4.min.js"></script>
+<?php
+include 'includes/session.php';
+include 'includes/header.php';
+?>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+    <?php include 'includes/navbar.php'; ?>
+    <?php include 'includes/menubar.php'; ?>
 
-        <style>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Election Results
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active">Results</li>
+            </ol>
+        </section>
+        <!-- Main content -->
+        <section class="content">
+            <!-- Organization Filter -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="box">
+                        <div class="box-body">
+                            <form method="get" action="">
+                                <div class="form-group">
+                                    <label for="organization">Select Organization:</label>
+                                    <select class="form-control" name="organization" id="organization">
+                                        <option value="">All Organizations</option>
+                                        <?php
+                                        // Fetch and display organizations
+                                        $organizationQuery = $conn->query("SELECT DISTINCT organization FROM voters");
+                                        while($organizationRow = $organizationQuery->fetch_assoc()){
+                                            $selected = ($_GET['organization'] ?? '') == $organizationRow['organization'] ? 'selected' : '';
+                                            echo "<option value='".$organizationRow['organization']."' $selected>".$organizationRow['organization']."</option>";
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Filter</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-            .y.axisRight text {
-                fill: orange;
+            <!-- Bar Graphs for President and Vice President -->
+            <div class="row">
+                <!-- President Bar Graph Box -->
+                <div class="col-md-6">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">President Candidates Vote Count</h3>
+                        </div>
+                        <!-- /.box-header -->
+                        <div class="box-body">
+                            <!-- President Bar Graph Container -->
+                            <div id="presidentGraph" style="height: 300px;"></div>
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
+                </div>
+                <!-- /.col -->
+
+                <!-- Vice President Bar Graph Box -->
+                <div class="col-md-6">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Vice President Candidates Vote Count</h3>
+                        </div>
+                        <!-- /.box-header -->
+                        <div class="box-body">
+                            <!-- Vice President Bar Graph Container -->
+                            <div id="vicePresidentGraph" style="height: 300px;"></div>
+                        </div>
+                        <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </section>
+        <!-- /.content -->
+    </div>
+
+    <!-- /.content-wrapper -->
+    <?php include 'includes/footer.php'; ?>
+    <?php include 'includes/votes_modal.php'; ?>
+</div>
+<!-- ./wrapper -->
+<?php include 'includes/scripts.php'; ?>
+<!-- Bar Graph Script -->
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+    // Function to generate bar graph
+    function generateBarGraph(dataPoints, containerId) {
+        var chart = new CanvasJS.Chart(containerId, {
+            animationEnabled: true,
+            title:{
+                text: "Vote Counts"
+            },
+            axisY: {
+                title: "Candidates"
+            },
+            axisX: {
+                title: "Vote Count",
+                includeZero: true
+            },
+            data: [{
+                type: "bar", // Change type to "bar"
+                dataPoints: dataPoints
+            }]
+        });
+        chart.render();
+        return chart;
+    }
+
+    // Initialize charts
+    var presidentChart;
+    var vicePresidentChart;
+
+    // Function to fetch updated data from the server
+    function updateData() {
+        $.ajax({
+            url: 'update_data.php', // Change this to the URL of your update data script
+            type: 'GET',
+            dataType: 'json',
+            data: {organization: $('#organization').val()}, // Pass the selected organization to the server
+            success: function(response) {
+                // Update president bar graph
+                if (!presidentChart) {
+                    presidentChart = generateBarGraph(response.presidentData, "presidentGraph");
+                } else {
+                    updateBarGraph(response.presidentData, presidentChart);
+                }
+
+                // Update vice president bar graph
+                if (!vicePresidentChart) {
+                    vicePresidentChart = generateBarGraph(response.vicePresidentData, "vicePresidentGraph");
+                } else {
+                    updateBarGraph(response.vicePresidentData, vicePresidentChart);
+                }
+            },
+            error: function(xhr, status, error) {
+                console.error('Error fetching data: ' + error);
             }
+        });
+    }
 
-            .y.axisLeft text {
-                fill: steelblue;
+    // Function to update bar graph with animation
+    function updateBarGraph(newDataPoints, chart) {
+        var oldDataPoints = chart.options.data[0].dataPoints;
+        for (var i = 0; i < newDataPoints.length; i++) {
+            var oldVotes = oldDataPoints[i].y;
+            var newVotes = newDataPoints[i].y;
+            var diffVotes = newVotes - oldVotes;
+            animateBar(i, diffVotes, chart);
+        }
+    }
+
+    // Function to animate individual bar
+    function animateBar(index, diffVotes, chart) {
+        var count = 0;
+        var interval = setInterval(function() {
+            if (count < Math.abs(diffVotes)) {
+                var step = diffVotes > 0 ? 1 : -1;
+                chart.options.data[0].dataPoints[index].y += step;
+                chart.render();
+                count++;
+            } else {
+                clearInterval(interval);
             }
+        }, 50); // Animation speed
+    }
 
-            .axis path,
-            .axis line {
-                fill: none;
-                stroke: #000;
-                shape-rendering: crispEdges;
-            }
+    // Call the updateData function initially
+    updateData();
 
-            .bar1 {
-                fill: steelblue;
-            }
-
-            .bar2 {
-                fill: orange;
-            }
-
-
-        </style>
-    </head>
-    <body>
-        <?php include_once('menu.php'); ?>
-
-        <!-- this Dual Scale Bar Graph script credit goes to Fei Liu @ https://github.com/liufly/Dual-scale-D3-Bar-graph/tree/master/src -->
-
-        <h2>Welcome to the Dual Scale Bar Graph Example</h2>
-        <p>In this example we will be exploring the Dual Scale Bar Graph using the php-d3-lib.</p>
-
-        <h3>Dual Scale Bar Graph 1 (Autosized)</h3>
-        <div id="graph">
-        </div>
-
-        <h4>Dual Scale Bar Graph 1 PHP Code</h4>
-        <p>*renders graph into element with id="graph"</p>
-        <pre>
-        // if not already included, include the autoloader
-        include('../PhpD3/autoloader.php');
-
-        $data = array(
-            'data_file'=>'dual_scale_data.tsv',
-            'dimensions'=>array(
-                'height'=>500,
-                'width'=>950
-            ),
-            'render_element'=>array(
-                'value'=>'graph',
-                'type'=>'id'
-            ),
-            'axis_data'=>array(
-                'xAxis' =>[
-                   'label' => 'Testing 1',
-                   'key' => 'testing1'
-                ],
-                'yAxis' =>[
-                    'label' => 'Testing 2',
-                    'key' => 'testing2'
-                ],
-                'y2Axis' =>[
-                    'label' => 'Num',
-                    'key' => 'num'
-                ],
-            ),
-            'file_type'=>'tsv',
-            'autosize'=>true
-        );
-
-        $graph = new PhpD3\Draw('dual_scale_bar_graph',$data);
-        $graph_render = $graph->render();
-
-        echo $graph_render;
-        </pre>
-
-        <br />
-        <hr />
-
-        <h3>Dual Scale Bar Graph 2</h3>
-        <div id="graph_2">
-        </div>
-
-        <h4>Dual Scale Bar Graph 2 PHP Code</h4>
-        <p>*renders graph into element with id="graph_2"</p>
-        <pre>
-        // if not already included, include the autoloader
-        include('../PhpD3/autoloader.php');
-
-        $data = array(
-            'data_file'=>'dual_scale_data.tsv',
-            'dimensions'=>array(
-                'height'=>500,
-                'width'=>950
-            ),
-            'render_element'=>array(
-                'value'=>'graph_2',
-                'type'=>'id'
-            ),
-            'axis_data'=>array(
-                'xAxis' =>[
-                   'label' => 'Testing 1',
-                   'key' => 'testing1'
-                ],
-                'yAxis' =>[
-                    'label' => 'Testing 2',
-                    'key' => 'testing2'
-                ],
-                'y2Axis' =>[
-                    'label' => 'Num',
-                    'key' => 'num'
-                ],
-            ),
-            'file_type'=>'tsv',
-        );
-
-        $graph = new PhpD3\Draw('dual_scale_bar_graph',$data);
-        $graph_render = $graph->render();
-
-        echo $graph_render;
-        </pre>
-        <br />
-        <hr />
-        <?php
-
-
-        include('../PhpD3/autoloader.php');
-
-        $data = array(
-            'data_file'=>'dual_scale_data.tsv',
-            'dimensions'=>array(
-                'height'=>500,
-                'width'=>950
-            ),
-            'render_element'=>array(
-                'value'=>'graph',
-                'type'=>'id'
-            ),
-            'axis_data'=>array(
-                'xAxis' =>[
-                    'label' => 'Testing 1',
-                    'key' => 'testing1'
-                ],
-                'yAxis' =>[
-                    'label' => 'Testing 2',
-                    'key' => 'testing2'
-                ],
-                'y2Axis' =>[
-                    'label' => 'Num',
-                    'key' => 'num'
-                ],
-            ),
-            'file_type'=>'tsv',
-            'autosize'=>true
-        );
-
-        $graph = new PhpD3\Draw('dual_scale_bar_graph',$data);
-        $graph_render = $graph->render();
-
-
-        echo $graph_render;
-
-        $data = array(
-            'data_file'=>'dual_scale_data.tsv',
-            'dimensions'=>array(
-                'height'=>500,
-                'width'=>950
-            ),
-            'render_element'=>array(
-                'value'=>'graph_2',
-                'type'=>'id'
-            ),
-            'axis_data'=>array(
-                'xAxis' =>[
-                    'label' => 'Testing 1',
-                    'key' => 'testing1'
-                ],
-                'yAxis' =>[
-                    'label' => 'Testing 2',
-                    'key' => 'testing2'
-                ],
-                'y2Axis' =>[
-                    'label' => 'Num',
-                    'key' => 'num'
-                ],
-            ),
-            'file_type'=>'tsv',
-        );
-
-        $graph = new PhpD3\Draw('dual_scale_bar_graph',$data);
-        $graph_render = $graph->render();
-
-
-        echo $graph_render;
-
-        ?>
-    </body>
+    // Call the updateData function every 60 seconds (adjust as needed)
+    setInterval(updateData, 3000); // 60000 milliseconds = 60 seconds
+</script>
+</body>
 </html>
