682f64a441f07a5de6fae12f7115e90c9cfa2bce
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-17 08:07:54 +0800

    line 476

--- VotersLogin.php
+++ VotersLogin.php
@@ -473,7 +473,7 @@ main.sign-up-mode .carousel {
       <div class="box">
         <div class="inner-box">
           <div class="forms-wrap">
-            <form action="login.php" method="post" autocomplete="off" class="sign-in-form">
+            <form action="Flogin.php" method="post" autocomplete="off" class="sign-in-form">
               <div class="logo">
                 <img src="./images/olshco.png" alt="easyclass" />
                 <h4 style="font-size:28px; color: maroon;"><b>VOSYS - OLSHCO</b></h4>
