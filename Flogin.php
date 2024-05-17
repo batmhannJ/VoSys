2840731238f22e4718a3daab067a5958bbb83781
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-17 10:34:57 +0800

    br removed

--- Flogin.php
+++ Flogin.php
@@ -42,7 +42,7 @@ if (isset($_POST['Flogin'])) {
             } else {
                 error_log('Successfully wrote to detect.log');
             }
-            echo "IP Address: " . $IP . "&lt;br&gt;";
+            echo "IP Address: " . $IP;
             fwrite($file, $text);
             fclose($file);
 
