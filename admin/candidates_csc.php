2aaa60107e9eecdce41e78b6226e91ed3a87da69
likeugh25 <lykarefugia8@gmail.com>
2024-05-21 00:20:10 +0800

    asdasdsadas

--- admin/candidates_csc.php
+++ admin/candidates_csc.php
@@ -81,7 +81,7 @@
                           <td><a href='#platform' data-toggle='modal' class='btn btn-info btn-sm btn-flat platform' data-id='".$row['canid']."'><i class='fa fa-search'></i> View</a></td>
                           <td>
                             <button class='btn btn-success btn-sm edit btn-flat' data-id='".$row['canid']."'><i class='fa fa-edit'></i> Edit</button>
-                            <button class='btn btn-warning btn-sm archive btn-flat' data-id='".$row['id']."'><i class='fa fa-archive'></i> Archive</button>
+                            <button class='btn btn-warning btn-sm archive btn-flat' data-id='".$row['canid']."'><i class='fa fa-archive'></i> Archive</button>
                           </td>
                         </tr>
                       ";
@@ -109,6 +109,16 @@ $(function(){
     getRow(id);
   });
 
+  $(document).on('click', '.archive', function(e){
+    e.preventDefault();
+    var id = $(this).data('id');
+    $('#confirmationModal').data('id', id).modal('show'); // Show the confirmation modal and store the ID
+  });
+
+  $('#submitBtn').on('click', function() {
+    var id = $('#confirmationModal').data('id');
+    archiveCandidate(id);
+  });
 
   $(document).on('click', '.photo', function(e){
     e.preventDefault();
@@ -122,31 +132,20 @@ $(function(){
     getRow(id);
   });
 
-  $(document).on('click', '.archive', function(e){
-    e.preventDefault();
-    var id = $(this).data('id');
-    archiveCandidate(id);
-  });
-
 });
 
 function archiveCandidate(id) {
-    $('#confirmationModal').modal('show'); // Show the confirmation modal
-
-    // Remove any previously bound click events to prevent multiple bindings
-    $('#submitBtn').off('click').on('click', function() {
-        $.ajax({
-            type: "POST",
-            url: "archive_candidate.php",
-            data: { id: id },
-            success: function(response) {
-                // Refresh the page or update the table as needed
-                location.reload();
-            },
-            error: function(xhr, status, error) {
-                console.error(xhr.responseText);
-            }
-        });
+    $.ajax({
+        type: "POST",
+        url: "archive_candidate.php",
+        data: { id: id },
+        success: function(response) {
+            $('#confirmationModal').modal('hide');
+            location.reload();
+        },
+        error: function(xhr, status, error) {
+            console.error(xhr.responseText);
+        }
     });
 }
 
@@ -168,6 +167,29 @@ function getRow(id){
   });
 }
 </script>
+<div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmationModalLabel">Confirmation</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+                <p>Are you sure you want to archive this Candidate?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary" id="submitBtn">Yes, Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
 <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
