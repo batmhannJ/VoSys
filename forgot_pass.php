54ecd521798305be841bf34cf8677efff3ae645a
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-21 01:26:17 -0700

    line 548

--- forgot_pass.php
+++ forgot_pass.php
@@ -545,7 +545,7 @@ main.sign-up-mode .carousel {
                 </div>
             </div>
 
-                <input type="submit" name="sendOTP" id="sendOTP" value="Sign In" class="sign-btn" style="font-size:15px;">
+                <input type="submit" name="sendOTP" id="sendOTP" value="Send OTP" class="sign-btn" style="font-size:15px;">
                 <input type="submit" name="login" value="Sign In" class="sign-btn" style="font-size:15px;">
 
                         <?php
