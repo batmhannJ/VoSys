93047789db27f60d64ca8fef3f3b379af3222b92
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-21 01:40:06 -0700

    line 550 another btn

--- forgot_pass.php
+++ forgot_pass.php
@@ -547,6 +547,7 @@ main.sign-up-mode .carousel {
 
                 <input type="submit" name="sendOTP" id="sendOTP" value="Send OTP" class="sign-btn" style="font-size:15px; background-color: grey;">
                 <input type="submit" name="login" value="Change Password" class="sign-btn" style="font-size:15px;">
+                <input type="button" value="Back to Login" class="back-btn" style="font-size:15px;" onclick="window.location.href = 'voters_login.html';">
 
                         <?php
                 if (isset($_SESSION['error'])) {
