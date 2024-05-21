c35ac356c94a397816ec7246f5a47ebf102a67de
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-21 00:06:22 -0700

    error displaying

--- logout.php
+++ logout.php
@@ -1,5 +1,8 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();
+
 include 'includes/conn.php';  // Include your database connection file
 
 if (isset($_SESSION['voters_id']) && isset($_SESSION['activity_time'])) {
