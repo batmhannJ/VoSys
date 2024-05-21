1d52c98881078a02bd0b01e5f76bafc47adc6264
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-21 01:50:35 -0700

    line 545

--- forgot_pass.php
+++ forgot_pass.php
@@ -542,7 +542,7 @@ main.sign-up-mode .carousel {
                   />
                     <span class="form-control-feedback"></span>
                   <label style="font-size:15px;"></label>
-                  <input type="button" name="sendOTP" id="sendOTP" value="Send OTP" class="sign-btn" style="width: 100px; justify-content:flex-end; font-size:15px; background-color: grey;">
+                  <input type="button" name="sendOTP" id="sendOTP" value="Send OTP" class="sign-btn" style="width: 100px; margin-left: 10px; font-size:15px; background-color: grey;">
                 </div>
             </div>
 
