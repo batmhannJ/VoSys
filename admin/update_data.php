833fef8ceed6afbdcca2acd96a2c5340087629ab
soobink1 <peronamarie9@gmail.com>
2024-05-22 11:30:11 +0800

    live

--- admin/update_data.php
+++ admin/update_data.php
@@ -8,10 +8,11 @@ function getVotes($category, $organizationFilter, $conn) {
     $data = array();
     $sql = "SELECT CONCAT(candidates.firstname, ' ', candidates.lastname) AS candidate_name, 
             COALESCE(COUNT(votes.candidate_id), 0) AS vote_count
-            FROM candidates
+            FROM categories 
+            LEFT JOIN candidates ON categories.id = candidates.category_id
             LEFT JOIN votes ON candidates.id = votes.candidate_id
             LEFT JOIN voters AS voters1 ON voters1.id = votes.voters_id 
-            WHERE candidates.category = '$category'
+            WHERE categories.name = '$category'
             $organizationFilter
             GROUP BY candidates.id";
     $query = $conn->query($sql);
