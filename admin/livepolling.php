7a37f71bccc1cee2589fbbff3d8c28eefc8125d9
soobink1 <peronamarie9@gmail.com>
2024-05-23 20:49:14 +0800

    vosys

--- admin/livepolling.php
+++ admin/livepolling.php
@@ -56,6 +56,114 @@ include 'includes/header.php';
                         </div>
                     </div>
                 </div>
+                <div class="box-body">
+                            <div id="vicePresidentInternalGraph" style="height: 300px;"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Vice President for External Affairs Candidates Vote Count</h3>
+                        </div>
+                        <div class="box-body">
+                            <div id="vicePresidentExternalGraph" style="height: 300px;"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Secretary Candidates Vote Count</h3>
+                        </div>
+                        <div class="box-body">
+                            <div id="secretaryGraph" style="height: 300px;"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Additional positions -->
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Treasurer Candidates Vote Count</h3>
+                        </div>
+                        <div class="box-body">
+                            <div id="treasurerGraph" style="height: 300px;"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Auditor Candidates Vote Count</h3>
+                        </div>
+                        <div class="box-body">
+                            <div id="auditorGraph" style="height: 300px;"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">P.R.O. Candidates Vote Count</h3>
+                        </div>
+                        <div class="box-body">
+                            <div id="proGraph" style="height: 300px;"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Dir. for Membership Candidates Vote Count</h3>
+                        </div>
+                        <div class="box-body">
+                            <div id="dirMembershipGraph" style="height: 300px;"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Dir. for Special Project Candidates Vote Count</h3>
+                        </div>
+                        <div class="box-body">
+                            <div id="dirSpecialProjectGraph" style="height: 300px;"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Block A 1st Year Representative Candidates Vote Count</h3>
+                        </div>
+                        <div class="box-body">
+                            <div id="blockA1stYearRepGraph" style="height: 300px;"></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="box">
+                        <div class="box-header with-border">
+                            <h3 class="box-title">Block B 1st Year Representative Candidates Vote Count</h3>
+                        </div>
+                        <div class="box-body">
+                            <div id="blockB1stYearRepGraph" style="height: 300px;"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
                 <!-- ... other existing categories ... -->
             </div>
 
@@ -171,10 +279,37 @@ include 'includes/header.php';
                 presidentChart.options.data[0].dataPoints = response.president;
                 presidentChart.render();
 
-                // Update existing category charts
-                // ...
+                vicePresidentInternalChart.options.data[0].dataPoints = response.vicePresidentInternal;
+                vicePresidentInternalChart.render();
 
-                // Update new category charts
+                vicePresidentExternalChart.options.data[0].dataPoints = response.vicePresidentExternal;
+                vicePresidentExternalChart.render();
+
+                secretaryChart.options.data[0].dataPoints = response.secretary;
+                secretaryChart.render();
+
+                treasurerChart.options.data[0].dataPoints = response.treasurer;
+                treasurerChart.render();
+
+                auditorChart.options.data[0].dataPoints = response.auditor;
+                auditorChart.render();
+
+                proChart.options.data[0].dataPoints = response.pro;
+                proChart.render();
+
+                dirMembershipChart.options.data[0].dataPoints = response.dirMembership;
+                dirMembershipChart.render();
+
+                dirSpecialProjectChart.options.data[0].dataPoints = response.dirSpecialProject;
+                dirSpecialProjectChart.render();
+
+                blockA1stYearRepChart.options.data[0].dataPoints = response.blockA1stYearRep;
+                blockA1stYearRepChart.render();
+
+                blockB1stYearRepChart.options.data[0].dataPoints = response.blockB1stYearRep;
+                blockB1stYearRepChart.render();
+
+                
                 blockA2ndYearRepChart.options.data[0].dataPoints = response.blockA2ndYearRep;
                 blockA2ndYearRepChart.render();
 
@@ -195,8 +330,17 @@ include 'includes/header.php';
             }
         });
     }
-
-    // Generate charts for new categories
+    var presidentChart = generateBarGraph([], "presidentGraph");
+    var vicePresidentInternalChart = generateBarGraph([], "vicePresidentInternalGraph");
+    var vicePresidentExternalChart = generateBarGraph([], "vicePresidentExternalGraph");
+    var secretaryChart = generateBarGraph([], "secretaryGraph");
+    var treasurerChart = generateBarGraph([], "treasurerGraph");
+    var auditorChart = generateBarGraph([], "auditorGraph");
+    var proChart = generateBarGraph([], "proGraph");
+    var dirMembershipChart = generateBarGraph([], "dirMembershipGraph");
+    var dirSpecialProjectChart = generateBarGraph([], "dirSpecialProjectGraph");
+    var blockA1stYearRepChart = generateBarGraph([], "blockA1stYearRepGraph");
+    var blockB1stYearRepChart = generateBarGraph([], "blockB1stYearRepGraph");
     var blockA2ndYearRepChart = generateBarGraph([], "blockA2ndYearRepGraph");
     var blockB2ndYearRepChart = generateBarGraph([], "blockB2ndYearRepGraph");
     var blockA3rdYearRepChart = generateBarGraph([], "blockA3rdYearRepGraph");
