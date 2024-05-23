58cce2a93d37a18d24886b873868fb790415e0ca
xantyyy <santypalma0@gmail.com>
2024-05-24 01:53:03 +0800

    asdasda

--- admin/includes/navbar_csc.php
+++ admin/includes/navbar_csc.php
@@ -1,15 +1,17 @@
 <header class="main-header">
-
+  <!-- Logo -->
   <a href="#" class="logo">
+    <!-- mini logo for sidebar mini 50x50 pixels -->
+    <span class="logo-mini"><b>CVS</span>
     <!-- logo for regular state and mobile devices -->
     <span class="logo-lg"><b>OLSHCO</b> Election </span>
   </a>
   <!-- Header Navbar: style can be found in header.less -->
   <nav class="navbar navbar-static-top">
     <!-- Sidebar toggle button-->
-    <!--<a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
       <span class="sr-only">Toggle navigation</span>
-    </a>-->
+    </a>
 
     <div class="navbar-custom-menu">
       <ul class="nav navbar-nav">
