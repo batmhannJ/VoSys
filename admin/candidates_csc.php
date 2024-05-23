f9a40fa1fb097034928be109abb9569a90c7a1ac
xantyyy <santypalma0@gmail.com>
2024-05-23 23:12:29 +0800

    asdasda

--- admin/candidates_csc.php
+++ admin/candidates_csc.php
@@ -83,7 +83,7 @@
                           <td>".$row['lastname']."</td>
                           <td><a href='#platform' data-toggle='modal' class='btn btn-info btn-sm btn-flat platform' data-id='".$row['canid']."'><i class='fa fa-search'></i> View</a></td>
                           <td>
-                            <button class='btn btn-success btn-sm edit btn-flat' data-id='".$row['canid']."'><i class='fa fa-edit'></i> Edit</button>
+                            <button class='btn btn-primary btn-sm edit btn-flat' data-id='".$row['canid']."'><i class='fa fa-edit'></i> Edit</button>
                             <button class='btn btn-warning btn-sm archive btn-flat' data-id='".$row['canid']."'><i class='fa fa-archive'></i> Archive</button>
                           </td>
                         </tr>
