20be10e25e3d0df03a6bc6fd97bd853638018362
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-24 03:31:26 +0800

    new code for getting csc output

--- admin/history_csc.php
+++ admin/history_csc.php
@@ -21,34 +21,27 @@ include 'includes/header_csc.php';
         </section>
         <!-- Main content -->
         <section class="content">
-            <!-- Organization Filter -->
-            <div class="row">
-                
-            </div>
-
             <!-- Ranking Boxes for Various Positions -->
             <div class="row">
                 <?php
                 // Function to display ranking table
-                function displayRankingTable($conn, $position, $organizationFilter) {
-                    $sql = "SELECT voters1.organization, CONCAT(candidates.firstname, ' ', candidates.lastname) AS candidate_name, 
+                function displayRankingTable($conn, $position) {
+                    $sql = "SELECT CONCAT(candidates.firstname, ' ', candidates.lastname) AS candidate_name, 
                             COALESCE(COUNT(votes_csc.candidate_id), 0) AS vote_count
                             FROM categories 
                             LEFT JOIN candidates ON categories.id = candidates.category_id
                             LEFT JOIN votes_csc ON candidates.id = votes_csc.candidate_id
-                            LEFT JOIN voters AS voters1 ON voters1.id = votes_csc.voters_id 
-                            WHERE voters1.organization != '' AND categories.name = '".$position."'".$organizationFilter."
-                            GROUP BY voters1.organization, candidates.id
+                            WHERE categories.name = '".$position."'
+                            GROUP BY candidates.id
                             ORDER BY vote_count DESC";
                     $query = $conn->query($sql);
                     $rank = 1;
                     while($row = $query->fetch_assoc()){
                         echo "
                             <tr>
-                            <td>".$rank."</td>
-                            <td>".$row['organization']."</td>
-                            <td>".$row['candidate_name']."</td>
-                            <td>".$row['vote_count']."</td>
+                                <td>".$rank."</td>
+                                <td>".$row['candidate_name']."</td>
+                                <td>".$row['vote_count']."</td>
                             </tr>";
                         $rank++;
                     }
@@ -71,8 +64,6 @@ include 'includes/header_csc.php';
                     'BSIT Representative'
                 ];
 
-                $organizationFilter = !empty($_GET['organization']) ? " AND voters1.organization = '".$_GET['organization']."'" : "";
-
                 // Loop through each position and display the ranking box
                 foreach ($positions as $position) {
                     echo "
@@ -86,13 +77,12 @@ include 'includes/header_csc.php';
                                     <thead>
                                     <tr>
                                         <th>Rank</th>
-                                        <th>Organization</th>
                                         <th>Candidate</th>
                                         <th>Vote Count</th>
                                     </tr>
                                     </thead>
                                     <tbody>";
-                    displayRankingTable($conn, $position, $organizationFilter);
+                    displayRankingTable($conn, $position);
                     echo "
                                     </tbody>
                                 </table>
@@ -105,7 +95,7 @@ include 'includes/header_csc.php';
                 <div class="row">
                     <div class="col-xs-12">
                         <span class="pull-right">
-                            <a href="export_results_csc.php?organization=<?php echo $_GET['organization'] ?? ''; ?>" class="btn btn-success btn-sm btn-flat"><span class="glyphicon glyphicon-print"></span> Export PDF</a>
+                            <a href="export_results_csc.php" class="btn btn-success btn-sm btn-flat"><span class="glyphicon glyphicon-print"></span> Export PDF</a>
                         </span>
                     </div>
                 </div>
