54f250cffe95591d95368b427bc3f91e3f4f11b1
soobink1 <peronamarie9@gmail.com>
2024-05-24 01:00:01 +0800

    org

--- admin/livepoll_csc.php
+++ admin/livepoll_csc.php
@@ -18,16 +18,7 @@ include 'includes/header_csc.php';
             </ol>
         </section>
 
-        <section class="content">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="box">
-                        <div class="box-body">
-                            <!-- Select Organization and Filter button removed -->
-                        </div>
-                    </div>
-                </div>
-            </div>
+        
 
             <div class="row">
                 <div class="col-md-3">
