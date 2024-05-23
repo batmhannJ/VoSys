8a55cfeb96fe4000e2da41b67e9fa80baa2c2fc0
xantyyy <santypalma0@gmail.com>
2024-05-23 23:36:25 +0800

    asdasdasd

--- admin/archive_csc.php
+++ admin/archive_csc.php
@@ -192,6 +192,7 @@
     <?php include 'includes/footer.php'; ?>
     <?php include 'includes/voters_modal.php'; ?>
     <?php include 'includes/restore_modal.php'; ?>
+    <?php include 'includes/candidates_modal_csc.php'; ?>
 
 <!-- Confirmation Modal for Delete -->
 <div id="deleteConfirmationModal" class="modal fade" role="dialog">
