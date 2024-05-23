0b99ab1f20bbd9cef7be00c713b23de94667214a
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-24 03:53:25 +0800

    removing icon

--- includes/ballot_modal.php
+++ includes/ballot_modal.php
@@ -66,7 +66,7 @@
         <div class="modal-content">
             <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
+                  </button>
               <h4 class="modal-title">Your Votes</h4>
             </div>
             <div class="modal-body">
