13bc6c4ab5bbe9e8ba6bcde28ecd78c7f1b248e1
soobink1 <peronamarie9@gmail.com>
2024-05-23 23:25:59 +0800

    center

--- admin/history.php
+++ admin/history.php
@@ -105,7 +105,7 @@ include 'includes/header.php';
                     <div class='col-md-12'>
                         <div class='box'>
                             <div class='box-header with-border'>
-                                <h3 class='box-title'>Ranking of ".$position." Candidates</h3>
+                                <h3 class='box-title center-title'>Ranking of ".$position." Candidates</h3>
                             </div>
                             <div class='box-body'>
                                 <table class='table table-bordered'>
