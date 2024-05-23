2705ada34e7219dfe6c677fd84630696c96ffb6c
xantyyy <santypalma0@gmail.com>
2024-05-24 03:24:56 +0800

    adszczxzc

--- admin/votes_csc.php
+++ admin/votes_csc.php
@@ -66,11 +66,11 @@
                     voters1.firstname AS votfirst, 
                     voters1.lastname AS votlast, 
                     voters1.organization AS org 
-                    FROM votes_csc 
-                    LEFT JOIN categories ON category.id=votes_csc.category_id 
-                    LEFT JOIN candidates ON candidates.id=votes_csc.candidate_id 
-                    LEFT JOIN voters AS voters1 ON voters1.id=votes_csc.voters_id 
-                    LEFT JOIN voters AS voters2 ON voters2.organization=votes_csc.organization 
+                    FROM votes
+                    LEFT JOIN categories ON category.id=votes.category_id 
+                    LEFT JOIN candidates ON candidates.id=votes.candidate_id 
+                    LEFT JOIN voters AS voters1 ON voters1.id=votes.voters_id 
+                    LEFT JOIN voters AS voters2 ON voters2.organization=votes.organization 
                     WHERE voters1.organization = 'CSC'
                     GROUP BY votes_csc.id
                     ORDER BY categories.priority ASC";
