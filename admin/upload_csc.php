911e816a0b84af22635be67b6d9f8ff55cf5d9a0
soobink1 <peronamarie9@gmail.com>
2024-05-24 22:41:03 +0800

    picture

--- admin/upload_csc.php
+++ admin/upload_csc.php
@@ -1,86 +1,50 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 include 'includes/session.php';
-include 'includes/conn.php';
 
-require 'vendor/autoload.php';
-require '/home/u247141684/domains/vosys.org/public_html/admin/PHPMailer/src/PHPMailer.php';
-require '/home/u247141684/domains/vosys.org/public_html/admin/PHPMailer/src/SMTP.php';
-require '/home/u247141684/domains/vosys.org/public_html/admin/PHPMailer/src/Exception.php';
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-use PhpOffice\PhpSpreadsheet\IOFactory;
-
-if (isset($_POST['save_excel_data'])) {
-    try {
-        $fileName = $_FILES['import_file']['name'];
-        $file_ext = pathinfo($fileName, PATHINFO_EXTENSION);
-
-        $allowed_ext = ['xls', 'csv', 'xlsx'];
-
-        if (!in_array($file_ext, $allowed_ext)) {
-            throw new Exception("Please upload a xls, csv or xlsx file.");
-        }
-
-        $inputFileNamePath = $_FILES['import_file']['tmp_name'];
-        $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileNamePath);
-        $data = $spreadsheet->getActiveSheet()->toArray();
-
-        $count = 0;
-        foreach ($data as $row) {
-            if ($count > 0) {
-                $firstname = $row['0'];
-                $lastname = $row['1'];
-                $email = $row['2'];
-                $organization = $row['3'];
-                $yearlvl = $row['4'];
-
-                if ($organization != 'CSC') {
-                    throw new Exception("Organization must be CSC.");
-                }
-
-                $set = '1234567890';
-                $voter = substr(str_shuffle($set), 0, 7);
-                $setPass = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[]{}|;:,.<>?";
-                $randomPassword = substr(str_shuffle($setPass), 0, 10);
-                $password = password_hash($randomPassword, PASSWORD_DEFAULT);
-
-                $studentQuery = "INSERT INTO voters (voters_id,genPass,password,firstname,lastname,email,organization,yearLvl) VALUES ('$voter','$randomPassword','$password','$firstname','$lastname','$email','$organization','$yearlvl')";
-                $result = mysqli_query($conn, $studentQuery);
-                $msg = true;
-
-                $mail = new PHPMailer();
-                // Remainder of the code...
-            } else {
-                $count = 1;
-            }
-        }
-
-        if (isset($msg)) {
-            $_SESSION['success'] = "Successfully Imported";
-            header('Location: voters_csc.php');
-            exit(0);
-        } else {
-            $_SESSION['message'] = "Not Imported";
-            header('Location: voters_csc.php');
-            exit(0);
-        }
-    } catch (Exception $e) {
-        $_SESSION['error'] = $e->getMessage();
-        header('Location: voters_csc.php');
-        exit(0);
-    }
-} else {
-    $_SESSION['message'] = "No data received.";
-    header('Location: voters_csc.php');
-    exit(0);
+$organizationFilter = "";
+if (!empty($_GET['organization'])) {
+    $organizationFilter = " AND voters1.organization = '" . $_GET['organization'] . "'";
 }
+
+// Function to fetch votes data
+function fetchVotes($conn, $category, $organizationFilter) {
+    $data = array();
+    $sql = "SELECT CONCAT(candidates.firstname, ' ', candidates.lastname) AS candidate_name, 
+            COALESCE(COUNT(votes_csc.candidate_id), 0) AS vote_count,
+            candidates.image_url
+            FROM categories 
+            LEFT JOIN candidates ON categories.id = candidates.category_id
+            LEFT JOIN votes_csc ON candidates.id = votes_csc.candidate_id
+            LEFT JOIN voters AS voters1 ON voters1.id = votes_csc.voters_id 
+            WHERE voters1.organization != '' AND categories.name = '$category'
+            $organizationFilter
+            GROUP BY candidates.id";
+    $query = $conn->query($sql);
+    while($row = $query->fetch_assoc()) {
+        $data[] = array(
+            "y" => intval($row['vote_count']),
+            "label" => $row['candidate_name'],
+            "image" => $row['image_url'] // Assuming you have a column for the image URL
+        );
+    }
+    return $data;
+}
+
+$response = array();
+$response['president'] = fetchVotes($conn, 'President', $organizationFilter);
+$response['vicePresident'] = fetchVotes($conn, 'Vice President', $organizationFilter);
+$response['secretary'] = fetchVotes($conn, 'Secretary', $organizationFilter);
+$response['treasurer'] = fetchVotes($conn, 'Treasurer', $organizationFilter);
+$response['auditor'] = fetchVotes($conn, 'Auditor', $organizationFilter);
+$response['publicInformationOfficer'] = fetchVotes($conn, 'Public Information Officer (P.R.O)', $organizationFilter);
+$response['businessManager'] = fetchVotes($conn, 'Business Manager', $organizationFilter);
+$response['beedRepresentative'] = fetchVotes($conn, 'BEED Representative', $organizationFilter);
+$response['bsedRepresentative'] = fetchVotes($conn, 'BSED Representative', $organizationFilter);
+$response['bshmRepresentative'] = fetchVotes($conn, 'BSHM Representative', $organizationFilter);
+$response['bsoadRepresentative'] = fetchVotes($conn, 'BSOAD Representative', $organizationFilter);
+$response['bsCrimRepresentative'] = fetchVotes($conn, 'BS CRIM Representative', $organizationFilter);
+$response['bsitRepresentative'] = fetchVotes($conn, 'BSIT Representative', $organizationFilter);
+
+header('Content-Type: application/json');
+echo json_encode($response);
 ?>
