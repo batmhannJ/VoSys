499211f5044f1566865a9ac04bd39d18aa284c22
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-26 22:53:39 +0800

    div arrangement

--- admin/export_results_csc.php
+++ admin/export_results_csc.php
@@ -126,10 +126,11 @@ tr:nth-child(odd) {
 <div class='header' style='display:flex;'>
 <div>
 <img src='images/logo.png' alt=Logo style='height: 100px; width: 100px;'>
+</div>
 <p style='font-family, cursive;'>Our Lady of the Sacred Heart College of Guimba, Inc.</p>
-</div>
-</div>
 <h1>2024 Election Results</h1>
+</div>
+
 <table>
     <thead>
     <tr>
