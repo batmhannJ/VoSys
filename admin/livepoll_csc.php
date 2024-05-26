ecf1dcb31bfff0ebe3fdf7424cb4ccaf651e678e
soobink1 <peronamarie9@gmail.com>
2024-05-26 10:07:45 +0800

    back

--- admin/livepoll_csc.php
+++ admin/livepoll_csc.php
@@ -64,10 +64,6 @@ include 'includes/header_csc.php';
             margin-right: 10px;
         }
 
-        .candidate-label {
-            margin-left: 10px;
-        }
-
         @media (max-width: 768px) {
             .candidate-image img {
                 width: 75px;
@@ -299,7 +295,7 @@ include 'includes/header_csc.php';
         // Update the image container
         var imageContainer = document.getElementById(imageContainerId);
         imageContainer.innerHTML = dataPoints.map(dataPoint =>
-            `<div class="candidate-image"><img src="${dataPoint.image}" alt="${dataPoint.label}" title="${dataPoint.label}"><span class="candidate-label">${dataPoint.label}</span></div>`
+            `<div class="candidate-image"><img src="${dataPoint.image}" alt="${dataPoint.label}" title="${dataPoint.label}"><span>${dataPoint.label}</span></div>`
         ).join('');
 
         var chart = new CanvasJS.Chart(containerId, {
@@ -355,7 +351,7 @@ include 'includes/header_csc.php';
         // Update the image container
         var imageContainer = document.getElementById(imageContainerId);
         imageContainer.innerHTML = newDataPoints.map(dataPoint =>
-            `<div class="candidate-image"><img src="${dataPoint.image}" alt="${dataPoint.label}" title="${dataPoint.label}"><span class="candidate-label">${dataPoint.label}</span></div>`
+            `<div class="candidate-image"><img src="${dataPoint.image}" alt="${dataPoint.label}" title="${dataPoint.label}"><span>${dataPoint.label}</span></div>`
         ).join('');
 
         chart.options.animationEnabled = true;
