29a897b4da6d0b4fc95aa27d011244450a5f45aa
soobink1 <peronamarie9@gmail.com>
2024-05-27 00:09:49 +0800

    back

--- admin/livepoll_csc.php
+++ admin/livepoll_csc.php
@@ -229,26 +229,6 @@ $(document).ready(function () {
         return false;
     });
 });
-        }
-    });
-}
-
-$(document).ready(function () {
-    fetchAndGenerateGraphs();
-
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('#back-to-top').fadeIn();
-        } else {
-            $('#back-to-top').fadeOut();
-        }
-    });
-
-    $('#back-to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 600);
-        return false;
-    });
-});
 </script>
 </body>
 </html>
