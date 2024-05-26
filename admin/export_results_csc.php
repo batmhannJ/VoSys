7c16cb9ba32a247df983111f73797e9120c78da6
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-27 00:07:21 +0800

    style

--- admin/export_results_csc.php
+++ admin/export_results_csc.php
@@ -115,9 +115,9 @@ tr:nth-child(odd) {
 }
 </style>
 <div class='header-container'>
-  <p class='school-name' style='font-size: 18px; font-weight: bold; margin-left: -10px;'>Our Lady of the Sacred Heart College of Guimba, Inc.</p>
-  <p class='report-title' style='font-size: 16px; margin-top: -500px;'>2024 Election Results</p>
-  <img src='images/logo.png' alt='Logo' style='height: 70px; width: 70px; float: left; top: -100px;'>
+  <img src='images/logo.png' alt='Logo' style='height: 70px; width: 70px; float: left;'>
+  <p class='school-name' style='font-size: 20px; font-weight: bold; margin-left: -10px;'>Our Lady of the Sacred Heart College of Guimba, Inc.</p>
+  <p class='report-title' style='font-size: 16px; margin-top: 50px;'>2024 Election Results</p>
 </div>
 <table>
     <thead>
