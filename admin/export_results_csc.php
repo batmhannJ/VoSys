a55ecffd3122b20139d5f11714c1fc5146e46107
batmhannJ <reyeshannahjoy82@gmail.com>
2024-05-27 00:08:03 +0800

    new

--- admin/export_results_csc.php
+++ admin/export_results_csc.php
@@ -116,7 +116,7 @@ tr:nth-child(odd) {
 </style>
 <div class='header-container'>
   <img src='images/logo.png' alt='Logo' style='height: 70px; width: 70px; float: left;'>
-  <p class='school-name' style='font-size: 20px; font-weight: bold; margin-left: -10px;'>Our Lady of the Sacred Heart College of Guimba, Inc.</p>
+  <p class='school-name' style='font-size: 20px; font-weight: bold; margin-left: -10px;'>Our Lady of the Sacred Heart College of Guimba, Inc.<br>Guimba, Nueva Ecija</p>
   <p class='report-title' style='font-size: 16px; margin-top: 50px;'>2024 Election Results</p>
 </div>
 <table>
