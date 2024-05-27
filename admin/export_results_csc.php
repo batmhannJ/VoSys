5788686422efa10ba9b921d9efa2e38326730133
LEVi19xx <charmainejoycecoloma02@gmail.com>
2024-05-27 20:49:18 +0800

    design

--- admin/export_results_csc.php
+++ admin/export_results_csc.php
@@ -186,26 +186,32 @@ $pdfContent .= "
 <p style='text-align: left;'><b>Voter Turnout:</b> {$voter_turnout}%</p>
 <br><br>
 <p><b>Signatures:</b></p>
-<div class='signature-block' style='display: flex;'>
+<div class='signature-block'>
   <span class='name'>HANNAH JOY REYES</span><br>
-  <span class='role'>Tabulator</span><br>
-  <span class='name'>CHARMAINE JOYCE COLOMA</span><br>
-  <span class='role'>Tabulator</span><br>
+  <span class='role'>Tabulator</span>
 </div>
-<div class='signature-block' style='display: flex;'>
+<div class='signature-block'>
+  <span class='name'>CHARMAINE JOYCE COLOMA</span><br>
+  <span class='role'>Tabulator</span>
+</div>
+<div class='signature-block'>
   <span class='name'>LYKA REFUGIA</span><br>
-  <span class='role'>Tabulator</span><br>
+  <span class='role'>Tabulator</span>
+</div>
+<div class='signature-block'>
   <span class='name'>MARIE LORAIN PERONA</span> <br>
-  <span class='role'>Tabulator</span><br>
+  <span class='role'>Tabulator</span>
+</div>
+<div class='signature-block'>
   <span class='name'>SANTY P. BALMORES</span> <br>
-  <span class='role'>Tabulator</span><br>
+  <span class='role'>Tabulator</span>
 </div>
 <div class='signature-block'>
   <span class='name'>LUIS B. TADENA</span> <br>
   <span class='role'>Head of COMELEC</span>
 </div>
 <div class='signature-block'>
-  <span class='name'>JESSICA MAE SALAZAE</span> <br>
+  <<span class='name'>JESSICA MAE C. SALAZA</span> <br>
   <span class='role'>Student Affair Officer</span>
 </div>
 ";
