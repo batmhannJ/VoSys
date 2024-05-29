412728c77aaecdf1c3502534d7c51a27f0919f8e
xantyyy <santypalma0@gmail.com>
2024-05-29 22:54:38 +0800

    asdcdasasd

--- admin/announce_delete.php
+++ admin/announce_delete.php
@@ -1,20 +1,20 @@
 <?php
-include 'includes/session.php';
+	include 'includes/session.php';
 
-if(isset($_POST['deleteAnnouncement'])){
-    $id = $_POST['id_announcement'];
+	if(isset($_POST['delete'])){
+		$id = $_POST['id'];
+		$sql = "DELETE FROM announcement WHERE id = '$id'";
+		if($conn->query($sql)){
+			$_SESSION['success'] = 'Announcement deleted successfully';
+		}
+		else{
+			$_SESSION['error'] = $conn->error;
+		}
+	}
+	else{
+		$_SESSION['error'] = 'Select item to delete first';
+	}
 
-    $sql = "DELETE FROM announcement WHERE id_announcement = '$id'";
-    if($conn->query($sql)){
-        $_SESSION['success'] = 'Announcement deleted successfully';
-    }
-    else{
-        $_SESSION['error'] = $conn->error;
-    }
-}
-else{
-    $_SESSION['error'] = 'Select item to delete first';
-}
-
-header('location: announcement.php');
+	header('location: announcement.php');
+	
 ?>
