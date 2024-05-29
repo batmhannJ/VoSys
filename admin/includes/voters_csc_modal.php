8bc55fa45a81127b0528db9b74629ba704760842
xantyyy <santypalma0@gmail.com>
2024-05-29 21:41:16 +0800

    adsadsasd

--- admin/includes/voters_csc_modal.php
+++ admin/includes/voters_csc_modal.php
@@ -203,7 +203,7 @@
         </button>
       </div>
       <div class="modal-body">
-        <p>Are you sure you want to restore this voter?</p>
+        <p>Are you sure you want to archive this voter?</p>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
