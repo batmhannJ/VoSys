a402b9c6c20e113351765cd7b9cec96490c748c8
batmhannJ <reyeshannahjoy82@gmail.com>
2024-06-03 14:18:18 +0800

    picture upload config

--- includes/profile_modal.php
+++ includes/profile_modal.php
@@ -71,7 +71,7 @@
                 <div class="form-group">
                     <label for="photo" class="col-sm-3 control-label">Photo:</label>
                     <div class="col-sm-9">
-                        <input type="file" id="photo" name="photo" accept=".png, .jpg, .jpeg, .csv">
+                        <input type="file" id="photo" name="photo" accept=".png, .jpg, .jpeg">
                     </div>
                 </div>
                 <hr>
