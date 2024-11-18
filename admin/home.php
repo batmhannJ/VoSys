61f07056b6f1763527f25178ad2b3cec597b4012
xantyyy <santypalma0@gmail.com>
2024-11-18 12:47:40 +0800

    request path

--- admin/home.php
+++ admin/home.php
@@ -133,15 +133,15 @@
                 echo "<h3>".$query->num_rows."</h3>";
               ?>
              
-              <p>Total Voters</p>
+              <p>Voter Request</p>
             </div>
             <div class="icon">
               <i class="fa fa-users"></i>
             </div>
-            <a href="voters.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="request.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
-        
+
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
--- admin/request.php
+++ admin/request.php
@@ -61,34 +61,6 @@
                     <th>Email</th>
                     <th>Tools</th>
                   </thead>
-                  <tbody>
-                    <?php
-                      $sql = "SELECT * FROM admin WHERE archived = FALSE AND organization != 'OSA'";
-                      $query = $conn->query($sql);
-                      $i = 1;
-                      while($row = $query->fetch_assoc()){
-                        $image = (!empty($row['photo'])) ? '../images/'.$row['photo'] : '../images/profile.jpg';
-                        echo "
-                          <tr>
-                          <td>".$i++."</td>
-                          <td>".$row['organization']."</td>
-                            <td>".$row['lastname']."</td>
-                            <td>".$row['firstname']."</td>
-                            <td>
-                              <img src='".$image."' width='30px' height='30px'>
-                              <a href='#edit_photo' data-toggle='modal' class='pull-right photo' data-id='".$row['id']."'><span class='fa fa-edit'></span></a>
-                            </td>
-                            <td>".$row['username']."</td>
-                            <td>".$row['email']."</td>
-                            <td>
-                              <button class='btn btn-primary btn-sm edit btn-flat' data-id='".$row['id']."'><i class='fa fa-edit'></i> Edit</button>
-                              <button class='btn btn-warning btn-sm archive btn-flat' data-id='".$row['id']."'><i class='fa fa-archive'></i> Archive</button>
-                            </td>
-                          </tr>
-                        ";
-                      }
-                    ?>
-                  </tbody>
                 </table>
               </div>
             </div>
@@ -99,89 +71,7 @@
   </div>
     
   <?php include 'includes/footer.php'; ?>
-  <?php include 'includes/sub_modal.php'; ?>
 </div>
 <?php include 'includes/scripts.php'; ?>
-<script>
-$(function(){
-  $(document).on('click', '.edit', function(e){
-    e.preventDefault();
-    $('#edit').modal('show');
-    var id = $(this).data('id');
-    getRow(id);
-  });
-
-  $(document).on('click', '.photo', function(e){
-    e.preventDefault();
-    var id = $(this).data('id');
-    getRow(id);
-  });
-
-  $(document).on('click', '.archive', function(e){ // Updated class name
-    e.preventDefault();
-    var id = $(this).data('id');
-    archiveAdmin(id);
-  });
-
-});
-
-function archiveAdmin(id) {
-    $('#confirmationModal').modal('show'); // Show the confirmation modal
-
-    $('#submitBtn').on('click', function() {
-        $.ajax({
-            type: "POST",
-            url: "archive_sub_admin.php",
-            data: { id: id },
-            success: function(response) {
-                // Refresh the page or update the table as needed
-                location.reload();
-            },
-            error: function(xhr, status, error) {
-                console.error(xhr.responseText);
-            }
-        });
-    });
-}
-
-function getRow(id){
-  $.ajax({
-    type: 'POST',
-    url: 'subAd_rows.php',
-    data: {id:id},
-    dataType: 'json',
-    success: function(response){
-      $('.id').val(response.id);
-      $('#edit_username').val(response.username);
-      $('#edit_firstname').val(response.firstname);
-      $('#edit_lastname').val(response.lastname);
-      $('#edit_email').val(response.email);
-      $('#edit_password').val(response.password);
-      $('.fullname').html(response.firstname+' '+response.lastname);
-    }
-  });
-}
-</script>
-<!-- Confirmation Modal -->
-<div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="confirmationModalLabel">Confirmation</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-
-            <div class="modal-body">
-                <p>Are you sure you want to archive this admin?</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                <button type="button" class="btn btn-primary" id="submitBtn">Yes, Submit</button>
-            </div>
-        </div>
-    </div>
-</div>
 </body>
 </html>
