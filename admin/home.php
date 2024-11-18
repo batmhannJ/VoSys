e664b87fb47b3196d9829dc43fbfa08c8edab416
xantyyy <santypalma0@gmail.com>
2024-11-18 12:44:55 +0800

    aaa

--- admin/home.php
+++ admin/home.php
@@ -205,6 +205,27 @@
         </div>
         <a href="turnout.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
     </div>
+
+    <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box">
+            <div class="inner">
+              <?php
+                $sql = "SELECT * FROM voters";
+                $query = $conn->query($sql);
+
+                echo "<h3>".$query->num_rows."</h3>";
+              ?>
+             
+              <p>Total Voters</p>
+            </div>
+            <div class="icon">
+              <i class="fa fa-users"></i>
+            </div>
+            <a href="voters.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        
 </div>
 </div>
 
