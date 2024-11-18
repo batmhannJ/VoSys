b7f25bba82694eff4eb9b415a8d91f95d5bf6718
xantyyy <santypalma0@gmail.com>
2024-11-18 14:34:33 +0800

    aaa

--- admin/home.php
+++ admin/home.php
@@ -122,26 +122,6 @@
           </div>
         </div>
 
-        <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
-          <div class="small-box">
-            <div class="inner">
-              <?php
-                $sql = "SELECT * FROM voters";
-                $query = $conn->query($sql);
-
-                echo "<h3>".$query->num_rows."</h3>";
-              ?>
-             
-              <p>Voter Request</p>
-            </div>
-            <div class="icon">
-              <i class="fa fa-users"></i>
-            </div>
-            <a href="request.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          </div>
-        </div>
-
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
