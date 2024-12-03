2700613df834f6081a021b0aaa4a3da9624c1d2c
xantyyy <santypalma0@gmail.com>
2024-12-03 22:12:46 +0800

    HAHAHAHAHA

--- admin/osa_forgotpass.php
+++ admin/osa_forgotpass.php
@@ -486,7 +486,7 @@ color: #333;
       <div class="box">
         <div class="inner-box">
           <div class="forms-wrap">
-            <form action="sendOTPForm" method="post" autocomplete="off" class="sign-in-form">
+            <form action="#" method="post" autocomplete="off" class="sign-in-form" id="sendOTPForm">
               <div class="logo">
                 <img src="./images/olshco.png" alt="easyclass" />
                 <h4 style="font-size:28px; color: maroon;"><b>VOSYS - OLSHCO</b></h4>
