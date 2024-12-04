e95c20ea5135f8b7e43521560431b356d12a8ae6
xantyyy <santypalma0@gmail.com>
2024-12-04 22:42:39 +0800

    zczc

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -2,12 +2,14 @@
 	session_start();
 	include 'includes/conn.php';
 
-	if(!isset($_SESSION['admin']) || trim($_SESSION['admin']) == ''){
+	if(!isset($_SESSION['admin']) || empty(trim($_SESSION['admin']) == '')){
 		header('location: index.php');
+		exit;
 	}
 
-	$sql = "SELECT * FROM admin WHERE id = '".$_SESSION['admin']."'";
-	$query = $conn->query($sql);
-	$user = $query->fetch_assoc();
+	$stmt = $conn->prepare("SELECT * FROM admin WHERE id = ?");
+	$stmt->bind_param("s", $_SESSION['admin']);
+	$stmt->execute();
+	$user = $stmt->get_result()->fetch_assoc();
 	
 ?>
