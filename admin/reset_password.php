564d058d2154069e8d0d1b3f577e2a1b1e2f1a7c
xantyyy <santypalma0@gmail.com>
2024-12-04 13:48:01 +0800

    zczc

--- admin/reset_password.php
+++ admin/reset_password.php
@@ -41,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $stmt->execute();
 
     $_SESSION['success'] = "Password successfully updated.";
-    header("Location: osa_forgotpass.php");
+    header("Location: defaultpass_admin.php");
     exit;
 }
 ?>
