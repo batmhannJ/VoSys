857b7d7f1a88f438ae40cbbb1c8f5f470e615131
LEVixx19 <charmainejoycecoloma02@gmail.com>
2024-12-09 01:44:49 +0800

    plat baba

--- jpcs_home.php
+++ jpcs_home.php
@@ -247,12 +247,14 @@ if (isset($voter['id'])) {
                                                                     data-max-vote="' . $maxVote . '">
                                                                 <img src="' . $image . '" alt="' . $crow['firstname'] . ' ' . $crow['lastname'] . '" class="candidate-image"> <br>
                                                                 <span class="candidate-name">' . $crow['firstname'] . ' ' . $crow['lastname'] . '</span> <br>
-                                                                <button type="button" class="btn btn-primary btn-flat platform-button" 
+                                                            
+                                                            </div>
+                                                            <div>
+                                                            button type="button" class="btn btn-primary btn-flat platform-button" 
                                                                         data-platform="' . $crow['platform'] . '" 
                                                                         data-fullname="' . $crow['firstname'] . ' ' . $crow['lastname'] . '">
                                                                     Platform
-                                                                </button>
-                                                            </div>';
+                                                                </button></div>';
                                                     
                                                     
 }                    
