d65a77ee7018daba868411818c078da8d3ba68d7
xantyyy <santypalma0@gmail.com>
2024-12-09 22:45:11 +0800

    change

--- admin/livepolling.php
+++ admin/livepolling.php
@@ -127,11 +127,13 @@
 
     <script src="path/to/jquery.min.js"></script>
     <script>
-        // Generate amCharts Bar Chart
+        // Generate amCharts Horizontal Bar Chart
         function generateGraph(dataPoints, containerId, imageContainerId, graphType) {
             var chart = am4core.create(containerId, am4charts.XYChart);
 
+            chart.paddingTop = 20;
             chart.paddingRight = 40;
+            chart.paddingBottom = 40;
 
             chart.data = dataPoints.map(dataPoint => ({
                 category: dataPoint.label,
@@ -139,19 +141,20 @@
                 image: dataPoint.image
             }));
 
-            var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+            var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
             categoryAxis.dataFields.category = "category";
+            categoryAxis.renderer.inversed = true; // Reverse category axis for horizontal bar
             categoryAxis.renderer.grid.template.location = 0;
             categoryAxis.renderer.minGridDistance = 30;
 
-            var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+            var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
             valueAxis.renderer.minGridDistance = 50;
 
             var series = chart.series.push(new am4charts.ColumnSeries());
-            series.dataFields.valueY = "value";
-            series.dataFields.categoryX = "category";
+            series.dataFields.valueX = "value";
+            series.dataFields.categoryY = "category";
             series.name = "Votes";
-            series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/] votes";
+            series.columns.template.tooltipText = "{categoryY}: [bold]{valueX}[/] votes";
             series.columns.template.fillOpacity = 0.8;
 
             var bullet = series.bullets.push(new am4charts.Bullet());
@@ -166,11 +169,6 @@
 
             categoryAxis.renderer.cellStartLocation = 0.1;
             categoryAxis.renderer.cellEndLocation = 0.9;
-
-            // Add labels inside bars
-            series.columns.template.adapter.add("fill", function (fill, target) {
-                return target.dataItem.index % 2 === 0 ? am4core.color("#3e77ff") : fill;
-            });
         }
 
         function fetchAndGenerateGraphs(organization) {
