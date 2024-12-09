4fa034e6c220bfb02e27cc6b45d5b974ca79aa8e
xantyyy <santypalma0@gmail.com>
2024-12-10 00:10:39 +0800

    zcasasd

--- admin/fetch_election_details.php
+++ admin/fetch_election_details.php
@@ -5,63 +5,67 @@ include 'includes/slugify.php';
 if (isset($_POST['election_id'])) {
     $election_id = $_POST['election_id'];
     
-    // Kunin ang mga kategorya ng election
     $sql = "SELECT * FROM categories WHERE election_id = '$election_id' ORDER BY priority ASC";
     $query = $conn->query($sql);
 
     $output = '';
-    $candidate = '';
 
     while ($row = $query->fetch_assoc()) {
-        // Kunin ang mga kandidato sa kasalukuyang kategorya
-        $sql = "SELECT * FROM candidates WHERE category_id='".$row['id']."' ORDER BY votes DESC";
+        $sql = "SELECT * FROM candidates WHERE category_id='" . $row['id'] . "'";
         $cquery = $conn->query($sql);
         
-        $highestVotes = 0; // Panatilihin ang pinakamataas na boto
-        $winner = '';      // Pangalan ng nanalo
-        
+        $candidates = [];
         while ($crow = $cquery->fetch_assoc()) {
-            $image = (!empty($crow['photo'])) ? '../images/'.$crow['photo'] : '../images/profile.jpg';
-            $votes = $crow['votes']; // Kabuuang boto ng kandidato
-            
-            // Tukuyin ang nanalo
-            if ($votes > $highestVotes) {
-                $highestVotes = $votes;
-                $winner = $crow['firstname'].' '.$crow['lastname'];
-            }
+            $total_votes = $crow['votes']; // Assuming you have a 'votes' column
+            $image = (!empty($crow['photo'])) ? '../images/' . $crow['photo'] : '../images/profile.jpg';
 
-            $candidate .= '
-                <li style="list-style: none; margin-bottom: 15px;">
-                    <img src="'.$image.'" height="100px" width="100px" style="margin-right: 10px;">
-                    <span class="cname">'.$crow['firstname'].' '.$crow['lastname'].'</span>
-                    <span class="badge badge-info" style="margin-left: 10px;">Total Votes: '.$votes.'</span>
+            // Store candidate data for sorting
+            $candidates[] = [
+                'id' => $crow['id'],
+                'name' => $crow['firstname'] . ' ' . $crow['lastname'],
+                'photo' => $image,
+                'votes' => $total_votes
+            ];
+        }
+
+        // Sort candidates by total votes in descending order
+        usort($candidates, function ($a, $b) {
+            return $b['votes'] - $a['votes'];
+        });
+
+        $candidate_list = '';
+        $is_winner_marked = false; // Track if the winner has been marked
+        foreach ($candidates as $candidate) {
+            $winner_label = (!$is_winner_marked) ? '<span class="label label-success">Winner</span>' : '';
+            $is_winner_marked = true; // Only the first candidate is marked as the winner
+
+            $candidate_list .= '
+                <li>
+                    <img src="' . $candidate['photo'] . '" height="100px" width="100px" class="clist">
+                    <span class="cname clist">' . $candidate['name'] . '</span>
+                    <span class="votes clist">Votes: ' . $candidate['votes'] . '</span>
+                    ' . $winner_label . '
                 </li>
             ';
         }
 
-        $instruct = 'Highest votes will determine the winner.';
-
-        // Ipakita ang kategorya at ang nanalo
         $output .= '
             <div class="row">
                 <div class="col-xs-12">
-                    <div class="box box-solid" id="'.$row['id'].'">
+                    <div class="box box-solid" id="' . $row['id'] . '">
                         <div class="box-header with-border">
-                            <h3 class="box-title"><b>'.$row['name'].'</b></h3>
+                            <h3 class="box-title"><b>' . $row['name'] . '</b></h3>
                         </div>
                         <div class="box-body">
-                            <p>'.$instruct.'</p>
+                            <p>Select only one candidate</p>
                             <div id="candidate_list">
-                                <ul>'.$candidate.'</ul>
+                                <ul>' . $candidate_list . '</ul>
                             </div>
-                            <p><b>Winner: '.$winner.'</b></p>
                         </div>
                     </div>
                 </div>
             </div>
         ';
-
-        $candidate = ''; // I-reset ang listahan ng kandidato para sa susunod na kategorya
     }
 
     echo json_encode($output);
