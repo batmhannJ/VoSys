5cae77c58632455899ae3d9abec7659e6a37fa06
batmhannJ <reyeshannahjoy82@gmail.com>
2024-12-09 22:48:03 +0800

    tanggalin si ex

--- includes/profile_modal.php
+++ includes/profile_modal.php
@@ -3,7 +3,6 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>
                 <h4 class="modal-title"><b>User Profile</b></h4>
             </div>
             <div class="modal-body">
