99add46bafcd1299b27ca3957ab3d4d4d8e7193e
batmhannJ <reyeshannahjoy82@gmail.com>
2024-12-10 02:51:33 +0800

    asdsa

--- admin/livepolling.php
+++ admin/livepolling.php
@@ -163,7 +163,7 @@
             series.columns.template.tooltipText = "{category}: [bold]{valueX} votes ({percentage}%)[/]";
 
             // **New Code to Control Bar Width**
-            series.columns.template.width = am4core.percent(80); // Increase bar width to 80% of available space
+            series.columns.template.width = am4core.percent(100); // Increase bar width to 80% of available space
 
             var labelBullet = series.bullets.push(new am4charts.LabelBullet());
             labelBullet.label.text = "{percentage}%";
