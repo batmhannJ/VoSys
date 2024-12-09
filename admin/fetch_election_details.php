5727c9005a14eeb244f7c6aec402581116ae7c50
xantyyy <santypalma0@gmail.com>
2024-12-10 00:06:47 +0800

    try

--- admin/fetch_election_details.php
+++ admin/fetch_election_details.php
@@ -5,6 +5,7 @@ include 'includes/slugify.php';
 if (isset($_POST['election_id'])) {
     $election_id = $_POST['election_id'];
     
+    // Kunin ang mga kategorya ng election
     $sql = "SELECT * FROM categories WHERE election_id = '$election_id' ORDER BY priority ASC";
     $query = $conn->query($sql);
 
@@ -12,25 +13,35 @@ if (isset($_POST['election_id'])) {
     $candidate = '';
 
     while ($row = $query->fetch_assoc()) {
-        $input = ($row['max_vote'] > 1) 
-            ? '<input type="checkbox" class="flat-red '.slugify($row['name']).'" name="'.slugify($row['name'])."[]".'">' 
-            : '<input type="radio" class="flat-red '.slugify($row['name']).'" name="'.slugify($row['name']).'">';
-        
-        $sql = "SELECT * FROM candidates WHERE category_id='".$row['id']."'";
+        // Kunin ang mga kandidato sa kasalukuyang kategorya
+        $sql = "SELECT * FROM candidates WHERE category_id='".$row['id']."' ORDER BY votes DESC";
         $cquery = $conn->query($sql);
+        
+        $highestVotes = 0; // Panatilihin ang pinakamataas na boto
+        $winner = '';      // Pangalan ng nanalo
+        
         while ($crow = $cquery->fetch_assoc()) {
             $image = (!empty($crow['photo'])) ? '../images/'.$crow['photo'] : '../images/profile.jpg';
+            $votes = $crow['votes']; // Kabuuang boto ng kandidato
+            
+            // Tukuyin ang nanalo
+            if ($votes > $highestVotes) {
+                $highestVotes = $votes;
+                $winner = $crow['firstname'].' '.$crow['lastname'];
+            }
+
             $candidate .= '
-                <li>
-                    '.$input.'<button class="btn btn-primary btn-sm btn-flat clist"><i class="fa fa-search"></i> Platform</button><img src="'.$image.'" height="100px" width="100px" class="clist"><span class="cname clist">'.$crow['firstname'].' '.$crow['lastname'].'</span>
+                <li style="list-style: none; margin-bottom: 15px;">
+                    <img src="'.$image.'" height="100px" width="100px" style="margin-right: 10px;">
+                    <span class="cname">'.$crow['firstname'].' '.$crow['lastname'].'</span>
+                    <span class="badge badge-info" style="margin-left: 10px;">Total Votes: '.$votes.'</span>
                 </li>
             ';
         }
 
-        $instruct = ($row['max_vote'] > 1) 
-            ? 'You may select up to '.$row['max_vote'].' candidates' 
-            : 'Select only one candidate';
+        $instruct = 'Highest votes will determine the winner.';
 
+        // Ipakita ang kategorya at ang nanalo
         $output .= '
             <div class="row">
                 <div class="col-xs-12">
@@ -43,13 +54,14 @@ if (isset($_POST['election_id'])) {
                             <div id="candidate_list">
                                 <ul>'.$candidate.'</ul>
                             </div>
+                            <p><b>Winner: '.$winner.'</b></p>
                         </div>
                     </div>
                 </div>
             </div>
         ';
 
-        $candidate = '';
+        $candidate = ''; // I-reset ang listahan ng kandidato para sa susunod na kategorya
     }
 
     echo json_encode($output);
