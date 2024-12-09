e596a05fbe87f2c0072601a93917419b40581581
xantyyy <santypalma0@gmail.com>
2024-12-09 23:37:25 +0800

    try

--- admin/archive_jpcs.php
+++ admin/archive_jpcs.php
@@ -153,34 +153,10 @@
       </div>
     </div>
   </div>
-  <section class="content">
-      <?php
-        if(isset($_SESSION['error'])){
-          echo "
-            <div class='alert alert-danger alert-dismissible'>
-              <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-              <h4><i class='icon fa fa-warning'></i> Error!</h4>
-              ".$_SESSION['error']."
-            </div>
-          ";
-          unset($_SESSION['error']);
-        }
-        if(isset($_SESSION['success'])){
-          echo "
-            <div class='alert alert-success alert-dismissible'>
-              <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-              <h4><i class='icon fa fa-check'></i> Success!</h4>
-              ".$_SESSION['success']."
-            </div>
-          ";
-          unset($_SESSION['success']);
-        }
-      ?>
-      <div class="row">
+  <div class="row">
         <div class="col-xs-10 col-xs-offset-1" id="content">
         </div>
       </div>
-    </section>
 </section>
 </div>
 
