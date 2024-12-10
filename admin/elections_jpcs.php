958fd9801dd0a627e2ce41c17d14a6594eccfe26
xantyyy <santypalma0@gmail.com>
2024-12-10 21:36:43 +0800

    qwe

--- admin/elections_jpcs.php
+++ admin/elections_jpcs.php
@@ -146,24 +146,22 @@
 </div>
 
 <!-- Deactivation Modal -->
-<div class="modal fade" id="deactivationModal" tabindex="-1" role="dialog" aria-labelledby="deactivationModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="deactivationModalLabel">Confirm Deactivation</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p>Are you sure you want to deactivate this election?</p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                <button type="button" id="deactivateBtn" class="btn btn-primary">Deactivate</button>
-            </div>
-        </div>
+<div class="modal fade" id="deactivationModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Deactivate Election</h5>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      <div class="modal-body">
+        <p>Are you sure you want to deactivate this election?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary" id="deactivationSubmit">Deactivate</button>
+      </div>
     </div>
+  </div>
 </div>
 
 </div>
