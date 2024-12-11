cbd8b67e5399603e32e39c905000bec3952b9d4e
LEVixx19 <charmainejoycecoloma02@gmail.com>
2024-12-12 06:28:02 +0800

    new command

--- admin/run_scan_ballot.php
+++ admin/run_scan_ballot.php
@@ -1,7 +1,7 @@
 <?php
 $pythonPath = 'C:\\Users\\Almira\\AppData\\Local\\Programs\\Python\\Python313\\python.exe';
 $scriptPath = 'C:\\Users\\Almira\\Desktop\\VoSys\\admin\\scan_ballot.py';
-$command = "\"$pythonPath\" \"$scriptPath\" 2>&1";
+$command = "cmd /c \"$pythonPath\" \"$scriptPath\"";
 
 $output = shell_exec($command);
 echo json_encode(['output' => $output]);
