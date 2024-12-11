74f8817d839b8a5941c6cd4fc1c2502acaaff694
LEVixx19 <charmainejoycecoloma02@gmail.com>
2024-12-12 06:20:03 +0800

    TRY AGAIN

--- admin/run_scan_ballot.php
+++ admin/run_scan_ballot.php
@@ -1,10 +1,5 @@
 <?php
-// Full path to Python executable
-$pythonPath = 'C:/Path/To/Python/python.exe';
-// Full path to the script
-$scriptPath = 'C:/Users/Almira/Desktop/VoSys/admin/scan_ballot.py';
-
 // Execute the Python script
-$output = shell_exec("$pythonPath $scriptPath 2>&1");
+$output = shell_exec('python /C:/Users/Almira/Desktop/VoSys/admin/scan_ballot.py 2>&1');
 echo json_encode(['output' => $output]);
 ?>
