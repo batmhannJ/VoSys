7e534130937b2453b242b6e282c0319a4c73cfc7
LEVixx19 <charmainejoycecoloma02@gmail.com>
2024-12-12 06:42:15 +0800

    old command

--- admin/run_scan_ballot.php
+++ admin/run_scan_ballot.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $output = shell_exec('python /home/u247141684/domains/vosys.org/public_html/admin/scan_ballot.py 2>&1'); // Adjust the path
+    $output = shell_exec('python3 /home/u247141684/domains/vosys.org/public_html/admin/scan_ballot.py 2>&1'); // Adjust the path
     echo json_encode(['status' => 'success', 'output' => $output]);
 } else {
     echo json_encode(['status' => 'error', 'message' => 'Invalid request']);
