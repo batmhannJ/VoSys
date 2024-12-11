14803397aab061bc7063aa992f7ebbe5696ab490
John Galicia <jgalicia@switchautomation.com>
2024-12-12 02:35:38 +0800

    300

--- admin/livepoll_jpcs.php
+++ admin/livepoll_jpcs.php
@@ -396,7 +396,7 @@
                                         </div>
                                         <div class='box-body'>
                                             <div class='chart-container'>
-                                                <div id='${category}Graph' style='height: 400px; width: 80%;'></div>
+                                                <div id='${category}Graph' style='height: 300px; width: 80%;'></div>
                                                 <div id='${category}Image' class='candidate-images'></div>
                                             </div>
                                         </div>
