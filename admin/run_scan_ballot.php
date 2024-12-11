991fd6c7e2751116c0160c7c658fe740c5c7ba46
LEVixx19 <charmainejoycecoloma02@gmail.com>
2024-12-12 06:26:31 +0800

    file directory

--- admin/run_scan_ballot.php
+++ admin/run_scan_ballot.php
@@ -1,15 +1,8 @@
 <?php
-$pythonPath = 'C:/Users/Almira/AppData/Local/Programs/Python/Python313/python.exe';
-$scriptPath = 'C:/Users/Almira/Desktop/VoSys/admin/scan_ballot.py';
-$command = "$pythonPath $scriptPath 2>&1";
-
-// Log the command for debugging
-file_put_contents('debug_command.txt', $command);
+$pythonPath = 'C:\\Users\\Almira\\AppData\\Local\\Programs\\Python\\Python313\\python.exe';
+$scriptPath = 'C:\\Users\\Almira\\Desktop\\VoSys\\admin\\scan_ballot.py';
+$command = "\"$pythonPath\" \"$scriptPath\" 2>&1";
 
 $output = shell_exec($command);
-
-// Log the output for debugging
-file_put_contents('debug_output.txt', $output);
-
 echo json_encode(['output' => $output]);
 ?>
