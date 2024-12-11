3b265aaa3f19475eb1ca8b32031302c674047bce
LEVixx19 <charmainejoycecoloma02@gmail.com>
2024-12-12 06:35:09 +0800

    DIRECTORY

--- admin/run_scan_ballot.php
+++ admin/run_scan_ballot.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $output = shell_exec('python3 C:\\Users\\Almira\\Desktop\\VoSys\\admin\\scan_ballot.py 2>&1'); // Adjust the path
+    $output = shell_exec('python3 C:\Users\Almira\Desktop\VoSys\admin 2>&1'); // Adjust the path
     echo json_encode(['status' => 'success', 'output' => $output]);
 } else {
     echo json_encode(['status' => 'error', 'message' => 'Invalid request']);
