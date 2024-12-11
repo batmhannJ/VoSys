2a9e3696108efbaf2c2d9d63380c371e28628863
LEVixx19 <charmainejoycecoloma02@gmail.com>
2024-12-12 06:20:46 +0800

    TRY AGAIN AND AGAIN

--- admin/run_scan_ballot.php
+++ admin/run_scan_ballot.php
@@ -1,5 +1,5 @@
 <?php
 // Execute the Python script
-$output = shell_exec('python /C:/Users/Almira/Desktop/VoSys/admin/scan_ballot.py 2>&1');
+$output = shell_exec('python3 /C:/Users/Almira/Desktop/VoSys/admin/scan_ballot.py 2>&1');
 echo json_encode(['output' => $output]);
 ?>
