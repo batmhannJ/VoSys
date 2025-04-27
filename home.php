6921deb3c66f1ed30b1938751816b4d027b56734
batmhannJ <reyeshannahjoy82@gmail.com>
2025-04-27 23:46:27 +0800

    color try

--- home.php
+++ home.php
@@ -355,7 +355,7 @@ if (isset($voter['id'])) {
                                 <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
-                                        <div class="modal-header">
+                                        <div class="modal-header" style="background-color: black">
                                             <h5 class="modal-title" id="confirmationModalLabel">Confirmation</h5>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                 <span aria-hidden="true">&times;</span>
